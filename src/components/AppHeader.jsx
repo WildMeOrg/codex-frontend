da33a7233f498155b503289e25dc9657f106d003
Ben Scheiner <brmscheiner@gmail.com>
2020-07-02 14:23:23 -0700

    Add app header popover

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -10,6 +10,8 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
 import MenuIcon from '@material-ui/icons/Menu';
+import Popover from '@material-ui/core/Popover';
+import Divider from '@material-ui/core/Divider';
 
 import { selectLogos } from '../modules/site/selectors';
 import { selectIsAuthenticated } from '../modules/app/selectors';
@@ -17,6 +19,9 @@ import { setLoginRedirect } from '../modules/app/actions';
 import ButtonLink from './ButtonLink';
 import Link from './Link';
 import AppDrawer from './AppDrawer';
+import shane from '../assets/shane.jpg';
+
+const popoverId = 'popover';
 
 export default function AppHeader() {
   const theme = useTheme();
@@ -26,6 +31,7 @@ export default function AppHeader() {
   const isAuthenticated = useSelector(selectIsAuthenticated);
   const logos = useSelector(selectLogos);
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = () => {
     setDrawerOpen(false);
@@ -51,8 +57,7 @@ export default function AppHeader() {
       <Toolbar
         style={{
           paddingRight: 24,
-          maxWidth: 1170,
-          width: '100%',
+          width: '90%',
           margin: '0 auto',
         }}
       >
@@ -86,16 +91,46 @@ export default function AppHeader() {
           </Link>
         </Typography>
         {isAuthenticated ? (
-          <Avatar
-            style={{
-              marginLeft: theme.spacing(1),
-              width: theme.spacing(4),
-              height: theme.spacing(4),
-            }}
-            variant="square"
-          >
-            1
-          </Avatar>
+          <>
+            <Avatar
+              style={{
+                marginLeft: theme.spacing(1),
+              }}
+              onClick={e => {
+                setAnchorEl(e.currentTarget);
+              }}
+              src={shane}
+            />
+            <Popover
+              id={popoverId}
+              open={Boolean(anchorEl)}
+              anchorEl={anchorEl}
+              onClose={() => setAnchorEl(null)}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              style={{ marginTop: 8 }}
+            >
+              <div style={{ padding: '12px 0' }}>
+                <Link href="/" noUnderline>
+                  <Typography style={{ margin: '0 20px' }}>
+                    View profile
+                  </Typography>
+                </Link>
+                <Divider style={{ margin: '12px 0' }} />
+                <Link href="/logout" noUnderline>
+                  <Typography style={{ margin: '0 20px' }}>
+                    Log out
+                  </Typography>
+                </Link>
+              </div>
+            </Popover>
+          </>
         ) : (
           <ButtonLink
             display="primary"
