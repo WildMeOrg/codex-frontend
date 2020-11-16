e39d5b9411f3548a81e91fbae770f202fc20b714
Ben Scheiner <brmscheiner@gmail.com>
2020-11-16 14:27:29 -0800

    Add powered by mark and instance logo

--- src/pages/splash/Splash.jsx
+++ src/pages/splash/Splash.jsx
@@ -6,12 +6,14 @@ import { useSelector } from 'react-redux';
 import { useTheme } from '@material-ui/core/styles';
 import ScrollIcon from '@material-ui/icons/KeyboardArrowUp';
 import Fab from '@material-ui/core/Fab';
+import Typography from '@material-ui/core/Typography';
 
 import useDocumentTitle from '../../hooks/useDocumentTitle';
 import { selectSiteSettings } from '../../modules/site/selectors';
 import ButtonLink from '../../components/ButtonLink';
 import ResponsiveText from '../../components/ResponsiveText';
 import useSiteSettings from '../../models/site/useSiteSettings';
+import wildMeLogo from '../../assets/wildme-logo.svg';
 import Trifold from './Trifold';
 import HowItWorks from './HowItWorks';
 import Testimonial from './Testimonial';
@@ -120,7 +122,7 @@ export default function Splash() {
             {get(newSiteSettings, ['site.general.tagline', 'value'])}
           </ResponsiveText>
           <ResponsiveText variant="subtitle1">
-            Upload your whale shark images. Help save the species.
+            Automated identification for cetaceans.
           </ResponsiveText>
           <ButtonLink
             display="marketing"
@@ -132,6 +134,43 @@ export default function Splash() {
           >
             <FormattedMessage id="REPORT_SIGHTINGS" />
           </ButtonLink>
+        </div>
+        <div
+          style={{ position: 'absolute', bottom: '10%', right: '5%' }}
+        >
+          <Typography
+            style={{
+              marginLeft: 6,
+              color: '#ddd',
+              fontStyle: 'italic',
+            }}
+          >
+            Powered by
+          </Typography>
+          <div style={{ display: 'flex', alignItems: 'flex-end' }}>
+            <img
+              width={72}
+              style={{ filter: 'invert(1)' }}
+              src={wildMeLogo}
+              alt="Wild Me logo"
+            />
+            <Typography
+              style={{ color: 'white', fontSize: 24, marginLeft: 8 }}
+            >
+              <span style={{ fontWeight: 'bold' }}>WILD</span>
+              <span>ME</span>
+              <span
+                style={{
+                  marginLeft: 4,
+                  fontStyle: 'italic',
+                  fontWeight: 300,
+                  textTransform: 'uppercase',
+                }}
+              >
+                sage
+              </span>
+            </Typography>
+          </div>
         </div>
       </div>
       <Trifold />
