c0b9efa6417fd6632c7764cf6a1c3152ddd19d7d
Ben Scheiner <brmscheiner@gmail.com>
2021-02-10 13:58:47 -0800

    Quick action entity menus

--- src/components/AuthenticatedAppHeader/NavTabs.jsx
+++ src/components/AuthenticatedAppHeader/NavTabs.jsx
@@ -4,24 +4,28 @@ import { useLocation, Link as RouterLink } from 'react-router-dom';
 
 import { useTheme } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Switch from '@material-ui/core/Switch';
+import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
 import Popover from '@material-ui/core/Popover';
-import HomeIcon from '@material-ui/icons/Home';
 import ProjectsIcon from '@material-ui/icons/GroupWork';
 import SightingsIcon from '@material-ui/icons/PhotoCamera';
+import AddIcon from '@material-ui/icons/Add';
 
+import FilterBar from '../FilterBar';
 import IndividualIcon from '../icons/IndividualIcon';
 import Button from '../Button';
+import ButtonLink from '../ButtonLink';
 import Text from '../Text';
 import Link from '../Link';
 import fakeProjects from './fakeProjects';
+import fakeIndividuals from './fakeIndividuals';
+import fakeSightings from './fakeSightings';
 
 function getTabValueFromLocation(location) {
   if (!location) return false;
@@ -33,7 +37,7 @@ function getTabValueFromLocation(location) {
   return false;
 }
 
-function NavButton({ titleId, href }) {
+function ProjectsButton({ titleId }) {
   const intl = useIntl();
   const theme = useTheme();
   const title = intl.formatMessage({ id: titleId });
@@ -81,7 +85,12 @@ function NavButton({ titleId, href }) {
           },
         }}
       >
-        <Text style={{ marginBottom: 4 }}>Current project</Text>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Text style={{ marginBottom: 4, fontWeight: 'bold' }}>Your projects</Text>
+          <IconButton size="small">
+            <AddIcon />
+          </IconButton>
+        </div>
         <List
           dense
           style={{ width: 260, height: 210, overflow: 'scroll' }}
@@ -100,7 +109,7 @@ function NavButton({ titleId, href }) {
                   <Avatar variant="square">A</Avatar>
                 </ListItemAvatar>
                 <ListItemText
-                  primary={<Link href={href}>{project.name}</Link>}
+                  primary={<Link href="/projects/noaa">{project.name}</Link>}
                   secondary={
                     <Text variant="caption">{project.pid}</Text>
                   }
@@ -116,26 +125,99 @@ function NavButton({ titleId, href }) {
             );
           })}
         </List>
-        <Button display="primary" style={{ margin: '12px 0' }}>
-          <FormattedMessage id="CREATE_A_PROJECT" />
-        </Button>
+        <ButtonLink onClick={handleClose} href="/projects" display="basic" style={{ margin: '4px 0 12px' }}>
+          View all projects
+        </ButtonLink>
       </Popover>
     </div>
   );
 }
 
-function NavTab({ titleId, href, Icon }) {
+function EntityButton({ titleId, entities, noAvatar }) {
   const intl = useIntl();
+  const theme = useTheme();
   const title = intl.formatMessage({ id: titleId });
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
-    <Tab
-      component={RouterLink}
-      to={href}
-      style={{ minWidth: 52 }}
-      aria-label={title}
-      label={title}
-      icon={<Icon />}
-    />
+    <div>
+      <Button
+        aria-describedby={id}
+        display="basic"
+        onClick={handleClick}
+        style={{ color: theme.palette.grey['100'] }}
+      >
+        {title}
+      </Button>
+      <Popover
+        onClose={handleClose}
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        PaperProps={{
+          style: {
+            width: 300,
+            padding: '20px 20px 8px 20px',
+          },
+        }}
+      >
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Text style={{ marginBottom: 4, fontWeight: 'bold' }}>{title}</Text>
+          <IconButton size="small">
+            <AddIcon />
+          </IconButton>
+        </div>
+        <FilterBar value="" style={{ with: 160, margin: '12px 0 4px 0' }} />
+        <List
+          dense
+          style={{ width: 260, height: 210, overflow: 'scroll' }}
+        >
+          {entities.map(entity => {
+            return (
+              <ListItem
+                style={{
+                  background: theme.palette.grey['100'],
+                }}
+              >
+                {!noAvatar && (
+                <ListItemAvatar>
+                <Avatar>A</Avatar>
+              </ListItemAvatar>
+                )}
+                <ListItemText
+                  primary={<Link href="/individuals/teddy">{entity.name}</Link>}
+                  secondary={
+                    <Text variant="caption">{entity.pid}</Text>
+                  }
+                />
+              </ListItem>
+            );
+          })}
+        </List>
+        <ButtonLink onClick={handleClose} href="/individuals" display="primary" style={{ margin: '8px 0 12px' }}>
+          {`Explore ${title}`}
+        </ButtonLink>
+      </Popover>
+    </div>
   );
 }
 
@@ -143,20 +225,22 @@ export default function NavTabs() {
   const location = useLocation();
   return (
     <Tabs value={getTabValueFromLocation(location)}>
-      <NavTab titleId="HOME" href="/" Icon={HomeIcon} value="home" />
-      <NavTab
+      <EntityButton
         titleId="INDIVIDUALS"
         href="/individuals"
         Icon={IndividualIcon}
         value="individuals"
+        entities={fakeIndividuals}
       />
-      <NavTab
+      <EntityButton
         titleId="SIGHTINGS"
         href="/sightings"
         Icon={SightingsIcon}
         value="sightings"
+        entities={fakeSightings}
+        noAvatar
       />
-      <NavButton
+      <ProjectsButton
         titleId="PROJECTS"
         href="/projects"
         Icon={ProjectsIcon}
---
+++ src/components/AuthenticatedAppHeader/fakeSightings.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 'abc',
+    name: 'June 12, 2020',
+    pid: 'Bermuda Coast',
+  },
+  {
+    id: 'def',
+    name: 'June 8, 2020',
+    pid: 'Coaba Coast',
+  },
+  {
+    id: 'gef',
+    name: 'June 8, 2020',
+    pid: 'Digglet Coast',
+  },
+  {
+    id: 'eas',
+    name: 'June 8, 2020',
+    pid: 'Bermuda Wavespren',
+  },
+  {
+    id: 'cwx',
+    name: 'August 5, 2019',
+    pid: 'Coaba Coast',
+  },
+  {
+    id: 'css',
+    name: 'June 4, 2020',
+    pid: 'Coaba Coast',
+  },
+];
