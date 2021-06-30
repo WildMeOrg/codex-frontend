dcb2bd7ea93ba32237f3515b76400b30332ee5c0
Mark <mark.aaron.fisher@gmail.com>
2021-06-30 16:37:27 -0700

    respond to BS feedback

---
+++ src/components/Alert.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import MuiAlert from '@material-ui/lab/Alert';
+import AlertTitle from '@material-ui/lab/AlertTitle';
+import Text from './Text';
+
+function Core({
+  descriptionId = '',
+  description,
+  descriptionValues,
+  alertChildren,
+  children,
+  ...rest
+}) {
+  return (
+    <MuiAlert {...rest}>
+      {children}
+      {descriptionId ? (
+        <FormattedMessage
+          id={descriptionId}
+          values={descriptionValues}
+        />
+      ) : (
+        description
+      )}
+      {alertChildren}
+    </MuiAlert>
+  );
+}
+
+export default function CustomAlert({
+  titleId = '',
+  titleTxt,
+  titleValues,
+  children: alertChildren,
+  ...rest
+}) {
+  const isTitle = titleId || titleTxt;
+  return (
+    <Core {...rest} alertChildren={alertChildren}>
+      {isTitle ? (
+        <AlertTitle>
+          {titleId ? (
+            <Text id={titleId} values={titleValues} />
+          ) : (
+            <Text values={titleValues}>{titleTxt}</Text>
+          )}
+        </AlertTitle>
+      ) : null}
+    </Core>
+  );
+}
