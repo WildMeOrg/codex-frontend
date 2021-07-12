d76c1844a8e94346083c6ffb6655dc4009e2634a
karen chan <karen@karen-chan.com>
2021-07-12 10:34:07 +0100

    Add src/constants/apiKeysTemplate.js back
    `apiKeysTemplate.js` was removed in commit
    "DEX-337 create AlertWrapper component" (86226532) and it was probably a
    mistake (not explained in commit message).  Add the file back because it
    is necessary for the build.

---
+++ src/constants/apiKeysTemplate.js
@@ -0,0 +1,8 @@
+/* To successfully run the app, you need to recreate a .gitignore'd file
+in this directory, apiKeys.js */
+
+export const googleMapsApiKey = '';
+
+export const sentryDsn = '';
+
+export const flatfileKey = '';
