b80ad9103dc1cbaeb5f34641deedcec457e46224
Ben Scheiner <brmscheiner@gmail.com>
2021-07-28 12:19:20 -0700

    Build species detector model multiselect

--- src/components/fields/edit/SelectionEditor.jsx
+++ src/components/fields/edit/SelectionEditor.jsx
@@ -11,6 +11,7 @@ import fieldTypes from '../../../constants/fieldTypesNew';
 import useLabel from '../../../hooks/useLabel';
 import useDescription from '../../../hooks/useDescription';
 import FormCore from './FormCore';
+import Text from '../../Text';
 
 function SelectionEditor(props) {
   const {
@@ -32,6 +33,7 @@ function SelectionEditor(props) {
   const label = useLabel(schema, true);
   const description = useDescription(schema);
   const showDescription = !minimalLabels && description;
+  const multiselect = schema.fieldType === fieldTypes.multiselect;
 
   const choices = get(schema, 'choices', []);
   const identifier = schema.id || schema.name;
@@ -46,12 +48,42 @@ function SelectionEditor(props) {
           onChange(e.target.value);
         }}
         value={value}
-        multiple={schema.fieldType === fieldTypes.multiselect}
+        multiple={multiselect}
+        renderValue={currentValue => {
+          if (multiselect) {
+            const selectedChoices = choices.filter(c =>
+              currentValue.includes(c.value),
+            );
+            const choiceLabels = selectedChoices.map(c =>
+              getLabel(c),
+            );
+            return choiceLabels.join(', ');
+          }
+          const selectedChoice = choices.find(
+            c => c.value === currentValue,
+          );
+          return getLabel(selectedChoice);
+        }}
         {...rest}
       >
         {choices.map(option => (
-          <MenuItem value={option.value} key={option.value}>
-            {getLabel(option)}
+          <MenuItem
+            value={option.value}
+            key={option.value}
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+            }}
+          >
+            <Text component="span">{getLabel(option)}</Text>
+            {option.description ? (
+              <Text component="span" variant="caption">
+                {option.description}
+              </Text>
+            ) : (
+              undefined
+            )}
           </MenuItem>
         ))}
       </Select>
