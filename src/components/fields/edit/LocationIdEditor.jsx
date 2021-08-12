e8f780502ad52d4318bff1ced09133c281133168
Mark <mark.aaron.fisher@gmail.com>
2021-08-12 06:23:59 -0700

    DEX-345 remove cruft and use option object array instead of string hack gymnastics thanks to getOptionLabel and respond to Ben code review feedback

--- src/components/fields/edit/LocationIdEditor.jsx
+++ src/components/fields/edit/LocationIdEditor.jsx
@@ -1,103 +1,73 @@
-import React, { useState } from 'react';
-import { useIntl } from 'react-intl';
-import { Autocomplete } from '@material-ui/lab';
-import { makeStyles } from '@material-ui/core/styles';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import TreeView from '@material-ui/lab/TreeView';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import TreeItem from '@material-ui/lab/TreeItem';
-import useLabel from '../../../hooks/useLabel';
-import useDescription from '../../../hooks/useDescription';
-import FormCore from './FormCore';
-import TextField from '@material-ui/core/TextField';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { Autocomplete } from '@material-ui/lab';
+import { TextField } from '@material-ui/core';
 import Text from '../../Text';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: 500,
-    '& > * + *': {
-      marginTop: theme.spacing(3),
-    },
-  },
-}));
 
 export default function LocationIdEditor(props) {
   const {
     schema,
     required,
-    value,
     onChange,
     width,
     minimalLabels = false,
     ...rest
   } = props;
 
-  const label = useLabel(schema);
-  const description = useDescription(schema);
-  const showDescription = !minimalLabels && description;
-
-  const intl = useIntl();
-  const [expanded, setExpanded] = useState([]);
-  const [selected, setSelected] = useState([]);
-
-  function renderLevel(leaves) {
-    if (!leaves || leaves.length === 0) return null;
-    return leaves.map(leaf => (
-      <TreeItem key={leaf.id} nodeId={leaf.id} label={leaf.name}>
-        {leaf.locationID && renderLevel(leaf.locationID)}
-      </TreeItem>
-    ));
-  }
-
   function getNumDescendents(targetChoice) {
     let result = 0;
     if (!targetChoice.locationID) return result;
-    const childCountsArr = targetChoice.locationID.map(child => {
-      return getNumDescendents(child);
-    });
-    result = targetChoice.locationID.length + childCountsArr.reduce((a, b) => a + b, 0);
+    const childCountsArr = targetChoice.locationID.map(child =>
+      getNumDescendents(child),
+    );
+    result =
+      targetChoice.locationID.length +
+      childCountsArr.reduce((a, b) => a + b, 0);
     return result;
   }
 
   function collapseChoices(choices, depth) {
     const result = choices.map(choice => {
+      const numDescendants = getNumDescendents(choice);
+      const numDescendantsAsString = numDescendants ? `(${numDescendants})` : '';
       if (!choice.locationID) {
-        return { depth, name: ' '.repeat(depth) + choice.name + (getNumDescendents(choice) ? '(' + getNumDescendents(choice).toString() + ')' : ''), id: choice.id };
+        return {
+          depth,
+          name: choice.name + numDescendantsAsString,
+          id: choice.id,
+        };
       }
-      const subchoices = [{ depth, name: ' '.repeat(depth) + choice.name + (getNumDescendents(choice) ? ' (' + getNumDescendents(choice).toString() + ')' : ''), id: choice.id }];
+      const subchoices = [
+        {
+          depth,
+          name: choice.name + numDescendantsAsString,
+          id: choice.id,
+        },
+      ];
       subchoices.push(collapseChoices(choice.locationID, depth + 1));
       return subchoices;
     });
     return result.flat(100);
   }
 
-  const handleToggle = (event, toggledNodes) => {
-    setExpanded(toggledNodes);
-  };
-
-  const handleSelect = (event, nodeIds) => {
-    if (!schema.multiselect) {
-      onChange(nodeIds);
-    } else {
-      onChange(nodeIds.filter(id => id !== 'IGNORE'));
-    }
-    setSelected(nodeIds);
-  };
-
   return (
     <Autocomplete
       multiple
-      options={collapseChoices(schema.choices, 0).map(result => result.name)}
-      renderOption={option => {
-        return (
-          <Text style={{ paddingLeft: (option.split(' ').length - option.trim().split(' ').length) * 10 }}>
-            {option}
-          </Text>
-        );
+      options={collapseChoices(schema.choices, 0)}
+      renderOption={option => (
+        <Text
+          style={{ paddingLeft: option.depth * 10 }}
+          value={option.id}
+        >
+          {option.name}
+        </Text>
+      )}
+      onChange={(event, newValue) => {
+        onChange(newValue[0].id);
       }}
-      renderInput={(params) => (
+      getOptionLabel={option => option.name}
+      getOptionSelected={(option, val) => option.id === val.id}
+      renderInput={params => (
         <TextField
           {...params}
           style={{ width: 280 }}
