0a2730e1b06f323c842ed60cd3fb4dfd5d17b895
Ben Scheiner <brmscheiner@gmail.com>
2021-10-08 13:55:52 -0700

    Add files back

---
+++ src/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { hot } from 'react-hot-loader/root';
+import * as Sentry from '@sentry/react';
+import { get } from 'lodash-es';
+
+import 'normalize.css';
+import '../fonts/fonts.css';
+import './styles/globalStyles.css';
+
+import { sentryDsn } from './constants/apiKeys';
+import pjson from '../package.json';
+import App from './App';
+import storeConfigs from './store';
+
+if (!__DEV__) {
+  Sentry.init({
+    dsn: sentryDsn,
+    release: `frontend@${get(pjson, 'release')}`,
+  });
+}
+
+const { store } = storeConfigs;
+
+const root = document.getElementById('root');
+
+const Main = hot(() => (
+  <>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>
+));
+
+const load = () => render(<Main />, root);
+
+load();
