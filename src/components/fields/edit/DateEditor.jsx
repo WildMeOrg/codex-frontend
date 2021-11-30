0c7e16076a063d2ea1829650ba3d68cb1e1684d5
Ben Scheiner <brmscheiner@gmail.com>
2021-11-29 17:27:18 -0800

    Address QA issues with report sighting form

--- src/components/fields/edit/DateEditor.jsx
+++ src/components/fields/edit/DateEditor.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import { useIntl } from 'react-intl';
 import DateFnsUtils from '@date-io/date-fns';
-import {
-  parseISO,
-  getHours,
-  setHours,
-  getMinutes,
-  setMinutes,
-} from 'date-fns';
+import { parseISO } from 'date-fns';
 import {
   MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-  KeyboardTimePicker,
+  KeyboardDateTimePicker,
 } from '@material-ui/pickers';
 import Text from '../../Text';
 import useLabel from '../../../hooks/useLabel';
@@ -40,42 +33,23 @@ export default function DateEditor(props) {
     <div>
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <KeyboardDatePicker
+          <KeyboardDateTimePicker
             disableToolbar
             disableFuture
             variant="inline"
             autoOk
-            format="yyyy-MM-dd" // US: MM/dd/yyyy
+            ampm={false} // US: true
+            format="yyyy-MM-dd HH:mm" // US: MM/dd/yyyy hh:mm
             margin="normal"
             id={`${label}-date-input`}
             label={intl.formatMessage({ id: 'SELECT_DATE' })}
             value={dateValue}
-            onChange={newDate => {
-              // Default to 12:00pm, leave hours and minutes unchanged if they are already set.
-              const hours = value ? getHours(dateValue) : 0;
-              const minutes = value ? getMinutes(dateValue) : 0;
-              newDate = setHours(newDate, hours);
-              newDate = setMinutes(newDate, minutes);
-              onChange(newDate);
-            }}
+            onChange={onChange}
             style={{ margin: 0, width }}
             KeyboardButtonProps={{
               'aria-label': intl.formatMessage({ id: 'CHANGE_DATE' }),
             }}
             {...rest}
-          />
-          <KeyboardTimePicker
-            variant="inline"
-            margin="normal"
-            format="HH:mm" // default for US
-            id={`${label}-time-input`}
-            label={intl.formatMessage({ id: 'SELECT_TIME' })}
-            value={dateValue}
-            onChange={onChange}
-            KeyboardButtonProps={{
-              'aria-label': intl.formatMessage({ id: 'CHANGE_TIME' }),
-            }}
-            style={{ width }}
           />
           {showDescription ? (
             <Text
