46f2484dea64a0ccdeeb7e060802b89fed481484
Mark <mark.aaron.fisher@gmail.com>
2021-11-30 14:15:18 -0800

    DEX-536 squashMe bounds around the markers drawn correctly and pins are clickable

--- src/components/maps/ManyPoints.jsx
+++ src/components/maps/ManyPoints.jsx
@@ -11,13 +11,21 @@ function createMapOptions() {
   };
 }
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Marker = ({ text }) => <div>{text}</div>;
 
 export default function ManyPoints({ latLongLabelArr }) {
   console.log('deleteMe got here');
   console.log('deleteMe latLongLabelArr is: ');
   console.log(latLongLabelArr);
   console.log('deleteMe hey there!!');
+  // const bounds = new window.google.maps.LatLngBounds();
+  // latLongLabelArr.forEach(entry => {
+  //   const latLng = new window.google.maps.LatLng(
+  //     get(entry, 'lat'),
+  //     get(entry, 'long'),
+  //   );
+  //   bounds.extend(latLng);
+  // });
   return (
     <GoogleMapReact
       options={createMapOptions}
@@ -25,18 +33,46 @@ export default function ManyPoints({ latLongLabelArr }) {
       defaultZoom={7}
       defaultCenter={{ lat: 0, lng: 0 }}
       yesIWantToUseGoogleMapApiInternals
+      onGoogleApiLoaded={({ map, maps }) => {
+        const bounds = new maps.LatLngBounds();
+        console.log('deleteMe GoogleMapReact render fired');
+        latLongLabelArr.forEach(entry => {
+          const currentLat = get(entry, 'lat');
+          const currentLong = get(entry, 'long');
+          if (currentLat && currentLong) {
+            const latLng = new maps.LatLng(currentLat, currentLong);
+            const marker = new maps.Marker({
+              position: latLng,
+              title: get(entry, 'text'),
+            });
+            marker.setMap(map);
+            const infoWindow = new maps.InfoWindow({
+              content: get(entry, 'text'),
+              map: map,
+            });
+            console.log('deleteMe infoWindow is: ');
+            console.log(infoWindow);
+
+            maps.event.addListener(marker, 'click', marker => {
+              console.log('deleteMe hey! Clicked!');
+              infoWindow.setContent(get(entry, 'text'));
+              infoWindow.open(map, marker);
+            });
+            bounds.extend(latLng);
+          }
+        });
+        map.fitBounds(bounds);
+        // maps.event.addListener(map, 'bounds_changed', function() {
+        //   console.log('deleteMe got here and bounds changed in the end');
+        // });
+      }}
     >
-      {latLongLabelArr.map(entry => {
-        console.log('deleteMe entry is: ');
-        console.log(entry);
+      {/* {latLongLabelArr.map(entry => {
         const tmpLat = get(entry, 'lat');
-        console.log('typeof tmpLat is: ');
-        console.log(typeof tmpLat);
         const tmpLong = get(entry, 'long');
-        console.log('typeof tmpLong is: ');
-        console.log(typeof tmpLong);
         return (
-          <AnyReactComponent
+          <Marker
+            text={get(entry, 'text', '')}
             style={{ outlineColor: 'red' }}
             key={get(entry, 'text', '')}
             name={get(entry, 'text', '')}
@@ -44,11 +80,7 @@ export default function ManyPoints({ latLongLabelArr }) {
             lng={get(entry, 'long')}
           />
         );
-        // position={{
-        //   lat: get(entry, 'lat'),
-        //   long: get(entry, 'long'),
-        // }}
-      })}
+      })} */}
     </GoogleMapReact>
   );
 }
