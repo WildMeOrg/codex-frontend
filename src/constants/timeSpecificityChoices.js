4c382803c10ff314d64f8a2d95247a9143f4aa50
Ben Scheiner <brmscheiner@gmail.com>
2022-02-09 16:53:52 -0800

    Change order of time precision dropdown

--- src/constants/timeSpecificityChoices.js
+++ src/constants/timeSpecificityChoices.js
@@ -1,19 +1,19 @@
 const timeSpecifityChoices = [
   {
-    label: 'I know the year',
-    value: 'year',
-  },
-  {
-    label: 'I know the month and year',
-    value: 'month',
+    label: 'I know the full date and time',
+    value: 'time',
   },
   {
     label: 'I know the full date',
     value: 'day',
   },
   {
-    label: 'I know the full date and time',
-    value: 'time',
+    label: 'I know the month and year',
+    value: 'month',
+  },
+  {
+    label: 'I know the year',
+    value: 'year',
   },
 ];
 
