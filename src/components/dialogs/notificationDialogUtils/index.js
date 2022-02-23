589f9e57fa665cc68ced2c130a0b34e3eef73079
Mark <mark.aaron.fisher@gmail.com>
2022-02-23 15:17:28 -0800

    DEX-727 add revoke access button and realize the request button for notifications is unnecessary for MVP

--- src/components/dialogs/notificationDialogUtils/index.js
+++ src/components/dialogs/notificationDialogUtils/index.js
@@ -1,17 +1,18 @@
 import NotificationViewButton from './NotificationViewButton';
 import NotificationCollaborationGrantAccessButton from './NotificationCollaborationGrantAccessButton';
 import NotificationDeclineCollaborationRequestButton from './NotificationDeclineCollaborationRequestButton';
+import NotificationCollaborationRevokeAccessButton from './NotificationCollaborationRevokeAccessButton';
 
 export const notificationButtonTypes = {
   view: 'view',
   grant: 'grant',
   decline: 'decline',
   revoke: 'revoke',
-  request: 'request',
 };
 
 export const notificationButtons = {
   [notificationButtonTypes.view]: NotificationViewButton,
   [notificationButtonTypes.grant]: NotificationCollaborationGrantAccessButton,
   [notificationButtonTypes.decline]: NotificationDeclineCollaborationRequestButton,
+  [notificationButtonTypes.revoke]: NotificationCollaborationRevokeAccessButton,
 };
