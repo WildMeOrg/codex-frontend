783c8dd02c7bae038ae7597bb81d0fe6dde6f048
Ben Scheiner <brmscheiner@gmail.com>
2022-03-02 16:57:39 -0800

    Add use mutate and fix request collaboration button

---
+++ src/hooks/useMutate.js
@@ -0,0 +1,116 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useQueryClient, useMutation } from 'react-query';
+
+const methods = {
+  put: 'put',
+  post: 'post',
+  patch: 'patch',
+  delete: 'delete',
+};
+
+function formatError(response) {
+  try {
+    return response?.error ? response.error.toJSON().message : null;
+  } catch (e) {
+    return 'Error could not be formatted as JSON';
+  }
+}
+
+export default function useMutate({
+  method,
+  url,
+  deriveUrl = Function.prototype,
+  data,
+  deriveData = Function.prototype,
+  params,
+  deriveParams = Function.prototype,
+  queryKeys = [],
+  deriveQueryKeys = () => [],
+  dataAccessor = result => result?.data?.data,
+  onSuccess = Function.prototype,
+  prependHoustonApiUrl = true,
+}) {
+  const queryClient = useQueryClient();
+  const [displayedError, setDisplayedError] = useState(null);
+  const [statusCode, setStatusCode] = useState(null);
+
+  const mutation = useMutation(async mutationArgs => {
+    const urlSuffix = url || deriveUrl(mutationArgs);
+    const completeUrl = prependHoustonApiUrl
+      ? `${__houston_url__}/api/v1${urlSuffix}`
+      : urlSuffix;
+
+    const response = await axios.request({
+      url: completeUrl,
+      // withCredentials: true,
+      method,
+      data: data || deriveData(mutationArgs),
+      params: params || deriveParams(mutationArgs),
+    });
+    const status = response?.status;
+    setStatusCode(status);
+    if (status === 200 || status === 204) {
+      const queryKeysFromArgs = deriveQueryKeys(mutationArgs);
+      const invalidationKeys = [...queryKeys, ...queryKeysFromArgs];
+      invalidationKeys.forEach(queryKey => {
+        queryClient.invalidateQueries(queryKey);
+      });
+      onSuccess(response);
+    }
+    return response;
+  });
+
+  const error = formatError(mutation);
+
+  useEffect(
+    () => {
+      if (error) {
+        setDisplayedError(error);
+      }
+    },
+    [error],
+  );
+
+  const mutate = mutationArgs => mutation.mutateAsync(mutationArgs);
+
+  return {
+    ...mutation,
+    mutate,
+    statusCode,
+    data: dataAccessor(mutation),
+    loading: mutation?.isLoading,
+    error: displayedError,
+    clearError: () => {
+      setDisplayedError(null);
+    },
+  };
+}
+
+export function usePut(args) {
+  return useMutate({
+    method: methods.put,
+    ...args,
+  });
+}
+
+export function usePost(args) {
+  return useMutate({
+    method: methods.post,
+    ...args,
+  });
+}
+
+export function usePatch(args) {
+  return useMutate({
+    method: methods.patch,
+    ...args,
+  });
+}
+
+export function useDelete(args) {
+  return useMutate({
+    method: methods.delete,
+    ...args,
+  });
+}
--- src/models/collaboration/useRequestCollaboration.js
+++ src/models/collaboration/useRequestCollaboration.js
@@ -1,67 +1,10 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { get } from 'lodash-es';
-import { useQueryClient } from 'react-query';
-
 import queryKeys from '../../constants/queryKeys';
-import { formatError } from '../../utils/formatters';
+import { usePost } from '../../hooks/useMutate';
 
 export default function useRequestCollaboration() {
-  const queryClient = useQueryClient();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [success, setSuccess] = useState(false);
-
-  const requestCollaboration = async userId => {
-    try {
-      setLoading(true);
-      setError(null);
-      const response = await axios({
-        url: `${__houston_url__}/api/v1/collaborations/`,
-        withCredentials: true,
-        method: 'post',
-        data: {
-          user_guid: userId,
-        },
-      });
-      const successful = get(response, 'status') === 200;
-      if (successful) {
-        setSuccess(true);
-        setError(null);
-        setLoading(false);
-        queryClient.invalidateQueries(queryKeys.me);
-        return successful;
-      }
-
-      const backendErrorMessage = response?.message;
-      const errorMessage =
-        backendErrorMessage || formatError(response);
-      setError(errorMessage);
-      setSuccess(false);
-      setLoading(false);
-      return null;
-    } catch (postError) {
-      const backendErrorMessage = get(postError, [
-        'response',
-        'data',
-        'message',
-      ]);
-      const errorMessage =
-        backendErrorMessage || formatError(postError);
-
-      setError(errorMessage);
-      setSuccess(false);
-      setLoading(false);
-      return null;
-    }
-  };
-
-  return {
-    requestCollaboration,
-    loading,
-    error,
-    setError,
-    success,
-    setSuccess,
-  };
+  return usePost({
+    url: '/collaborations/',
+    deriveData: ({ userGuid }) => ({ user_guid: userGuid }),
+    queryKeys: [queryKeys.me],
+  });
 }
