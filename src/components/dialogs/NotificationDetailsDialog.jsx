6d019135292765bbdc5a1d1c2e4a72d06b4c2c05
Mark <mark.aaron.fisher@gmail.com>
2022-03-14 12:57:50 -0700

    DEX-663 respond to code review feedback and DRY up notification props

--- src/components/dialogs/NotificationDetailsDialog.jsx
+++ src/components/dialogs/NotificationDetailsDialog.jsx
@@ -9,6 +9,7 @@ import StandardDialog from '../StandardDialog';
 import Text from '../Text';
 import Button from '../Button';
 import CustomAlert from '../Alert';
+import { getNotificationProps } from '../../utils/notificationUtils';
 
 export default function NotificationDetailsDialog({
   open,
@@ -25,31 +26,13 @@ export default function NotificationDetailsDialog({
     onClose();
   };
 
-  const senderName = get(notification, 'sender_name', 'Unnamed User');
-  let user1Name = get(
-    notification,
-    ['message_values', 'user1_name'],
-    'Unnamed User',
-  );
-  if (user1Name === '') user1Name = 'Unnamed User';
-  let user2Name = get(
-    notification,
-    ['message_values', 'user2_name'],
-    'Unnamed User',
-  );
-  if (user2Name === '') user2Name = 'Unnamed User';
-  const individual1Names = notification?.names || []; // TODO flesh out more once this is included in notifications DEX-739
-  const individual1NicknameObject = individual1Names.find(
-    n => n.context === 'nickname',
-  );
-  const individual1Nickname =
-    individual1NicknameObject?.value || 'Unnamed individual';
-  const individual2Names = notification?.names || []; // TODO flesh out more once this is included in notifications DEX-739
-  const individual2NicknameObject = individual2Names.find(
-    n => n.context === 'nickname',
-  );
-  const individual2Nickname =
-    individual2NicknameObject?.value || 'Unnamed individual';
+  const {
+    senderName,
+    user1Name,
+    user2Name,
+    individual1Nickname,
+    individual2Nickname,
+  } = getNotificationProps(notification);
 
   return (
     <StandardDialog
--- src/pages/notifications/Notifications.jsx
+++ src/pages/notifications/Notifications.jsx
@@ -24,6 +24,7 @@ import { calculatePrettyTimeElapsedSince } from '../../utils/formatters';
 import { notificationSchema } from '../../constants/notificationSchema';
 import { notificationTypes } from '../../components/dialogs/notificationDialogUtils';
 import queryKeys from '../../constants/queryKeys';
+import { getNotificationProps } from '../../utils/notificationUtils';
 
 export default function Notifications() {
   const intl = useIntl();
@@ -86,35 +87,13 @@ export default function Notifications() {
                   notificationType,
                 );
                 const read = get(notification, 'is_read', false);
-                const senderName = get(
-                  notification,
-                  'sender_name',
-                  'Unnamed User',
-                );
-                const user1Name = get(
-                  notification,
-                  ['message_values', 'user1_name'],
-                  'Unnamed User',
-                );
-                const user2Name = get(
-                  notification,
-                  ['message_values', 'user2_name'],
-                  'Unnamed User',
-                );
-                const individual1Names = notification?.names || []; // TODO flesh out more once this is included in notifications DEX-739
-                const individual1NicknameObject = individual1Names.find(
-                  n => n.context === 'nickname',
-                );
-                const individual1Nickname =
-                  individual1NicknameObject?.value ||
-                  'Unnamed individual';
-                const individual2Names = notification?.names || []; // TODO flesh out more once this is included in notifications DEX-739
-                const individual2NicknameObject = individual2Names.find(
-                  n => n.context === 'nickname',
-                );
-                const individual2Nickname =
-                  individual2NicknameObject?.value ||
-                  'Unnamed individual';
+                const {
+                  senderName,
+                  user1Name,
+                  user2Name,
+                  individual1Nickname,
+                  individual2Nickname,
+                } = getNotificationProps(notification);
                 const createdDate = notification?.created;
                 const timeSince = calculatePrettyTimeElapsedSince(
                   createdDate,
