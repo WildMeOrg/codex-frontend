610b2e6e206df83207fa8962daf0df80f314c0fe
Mark <mark.aaron.fisher@gmail.com>
2022-03-28 12:30:27 -0700

    test new fake asset

--- src/pages/individual/fakeAssets.js
+++ src/pages/individual/fakeAssets.js
@@ -2,7 +2,7 @@ const fakeAssets = [
   {
     id: 1,
     src:
-      'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
+      'https://i.etsystatic.com/15105231/r/il/ccb5ee/2368432547/il_1588xN.2368432547_ckhh.jpg',
     filename: 'Zebra',
   },
   {
