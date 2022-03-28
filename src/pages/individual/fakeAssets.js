b559b41d94d614326d8d683f250aed4de8fe56a9
Mark <mark.aaron.fisher@gmail.com>
2022-03-28 12:34:25 -0700

    DEX-843 test with four images

--- src/pages/individual/fakeAssets.js
+++ src/pages/individual/fakeAssets.js
@@ -23,35 +23,35 @@ const fakeAssets = [
       'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
     filename: 'Zebra',
   },
-  {
-    id: 5,
-    src:
-      'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
-    filename: 'Zebra',
-  },
-  {
-    id: 6,
-    src:
-      'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
-    filename: 'Zebra',
-  },
-  {
-    id: 7,
-    src:
-      'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
-    filename: 'Zebra',
-  },
-  {
-    id: 8,
-    src:
-      'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
-    filename: 'Zebra',
-  },
-  {
-    id: 9,
-    src:
-      'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
-    filename: 'Zebra',
-  },
+  // {
+  //   id: 5,
+  //   src:
+  //     'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
+  //   filename: 'Zebra',
+  // },
+  // {
+  //   id: 6,
+  //   src:
+  //     'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
+  //   filename: 'Zebra',
+  // },
+  // {
+  //   id: 7,
+  //   src:
+  //     'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
+  //   filename: 'Zebra',
+  // },
+  // {
+  //   id: 8,
+  //   src:
+  //     'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
+  //   filename: 'Zebra',
+  // },
+  // {
+  //   id: 9,
+  //   src:
+  //     'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
+  //   filename: 'Zebra',
+  // },
 ];
 export default fakeAssets;
