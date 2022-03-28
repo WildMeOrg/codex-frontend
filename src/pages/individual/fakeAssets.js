d4b4ba8429027ff0a28f63f19939cbbb2190ce74
Mark <mark.aaron.fisher@gmail.com>
2022-03-28 12:41:02 -0700

    DEX-843 test with 11 images

--- src/pages/individual/fakeAssets.js
+++ src/pages/individual/fakeAssets.js
@@ -14,44 +14,56 @@ const fakeAssets = [
   {
     id: 3,
     src:
-      'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
+      'https://m.media-amazon.com/images/I/81Rtj00tesL._AC_SX679_.jpg',
     filename: 'Zebra',
   },
   {
     id: 4,
     src:
-      'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
+      'https://secure.img1-fg.wfcdn.com/im/22626939/resize-h445%5Ecompr-r85/1816/181634637/Melina+Handmade+Machine+Woven+Area+Rug+in+Black.jpg',
     filename: 'Zebra',
   },
-  // {
-  //   id: 5,
-  //   src:
-  //     'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
-  //   filename: 'Zebra',
-  // },
-  // {
-  //   id: 6,
-  //   src:
-  //     'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
-  //   filename: 'Zebra',
-  // },
-  // {
-  //   id: 7,
-  //   src:
-  //     'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
-  //   filename: 'Zebra',
-  // },
-  // {
-  //   id: 8,
-  //   src:
-  //     'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
-  //   filename: 'Zebra',
-  // },
-  // {
-  //   id: 9,
-  //   src:
-  //     'http://prod-upp-image-read.ft.com/c963f918-05a5-11e7-aa5b-6bb07f5c8e12',
-  //   filename: 'Zebra',
-  // },
+  {
+    id: 5,
+    src:
+      'https://secure.img1-fg.wfcdn.com/im/78793717/resize-h445%5Ecompr-r85/5395/53951318/Porcupine+Luxury+Throw.jpg',
+    filename: 'Zebra',
+  },
+  {
+    id: 6,
+    src:
+      'https://m.media-amazon.com/images/I/71HPcemKTnL._AC_UL320_.jpg',
+    filename: 'Zebra',
+  },
+  {
+    id: 7,
+    src:
+      'https://i.etsystatic.com/23516277/r/il/712eef/2713684810/il_340x270.2713684810_fbiu.jpg',
+    filename: 'Zebra',
+  },
+  {
+    id: 8,
+    src:
+      'https://i.pinimg.com/736x/d1/d1/97/d1d19754db099dfdf732190575cb0d71.jpg',
+    filename: 'Zebra',
+  },
+  {
+    id: 9,
+    src:
+      'https://cdn.shopify.com/s/files/1/0535/9445/6238/products/tricolor634x634_SHARPENED_600x.jpg?v=1625856172',
+    filename: 'Zebra',
+  },
+  {
+    id: 10,
+    src:
+      'http://cdn.cnn.com/cnnnext/dam/assets/191008164033-01-fake-fur-leopard.jpg',
+    filename: 'Zebra',
+  },
+  {
+    id: 11,
+    src:
+      'https://inhabitat.com/wp-content/blogs.dir/1/files/2010/07/recycled-bear-skin-rug-6.jpg',
+    filename: 'Zebra',
+  },
 ];
 export default fakeAssets;
