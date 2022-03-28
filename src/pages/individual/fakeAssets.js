233460776218fcd6c8287e891ced09323ef26323
Mark <mark.aaron.fisher@gmail.com>
2022-03-28 13:40:52 -0700

    DEX-863 fix bug in fakeAssets

--- src/pages/individual/fakeAssets.js
+++ src/pages/individual/fakeAssets.js
@@ -1,66 +1,66 @@
 const fakeAssets = [
   {
-    id: 1,
+    number: 1,
     src:
       'https://i.etsystatic.com/15105231/r/il/ccb5ee/2368432547/il_1588xN.2368432547_ckhh.jpg',
     alt: 'Zebra',
   },
   {
-    id: 2,
+    number: 2,
     src:
       'https://i.etsystatic.com/13815421/r/il/67160e/2957884734/il_1588xN.2957884734_fj41.jpg',
     alt: 'Zebra',
   },
   {
-    id: 3,
+    number: 3,
     src:
       'https://m.media-amazon.com/images/I/81Rtj00tesL._AC_SX679_.jpg',
     alt: 'Zebra',
   },
   {
-    id: 4,
+    number: 4,
     src:
       'https://secure.img1-fg.wfcdn.com/im/22626939/resize-h445%5Ecompr-r85/1816/181634637/Melina+Handmade+Machine+Woven+Area+Rug+in+Black.jpg',
     alt: 'Zebra',
   },
   {
-    id: 5,
+    number: 5,
     src:
       'https://secure.img1-fg.wfcdn.com/im/78793717/resize-h445%5Ecompr-r85/5395/53951318/Porcupine+Luxury+Throw.jpg',
     alt: 'Zebra',
   },
   {
-    id: 6,
+    number: 6,
     src:
       'https://m.media-amazon.com/images/I/71HPcemKTnL._AC_UL320_.jpg',
     alt: 'Zebra',
   },
   {
-    id: 7,
+    number: 7,
     src:
       'https://i.etsystatic.com/23516277/r/il/712eef/2713684810/il_340x270.2713684810_fbiu.jpg',
     alt: 'Zebra',
   },
   {
-    id: 8,
+    number: 8,
     src:
       'https://i.pinimg.com/736x/d1/d1/97/d1d19754db099dfdf732190575cb0d71.jpg',
     alt: 'Zebra',
   },
   {
-    id: 9,
+    number: 9,
     src:
       'https://cdn.shopify.com/s/files/1/0535/9445/6238/products/tricolor634x634_SHARPENED_600x.jpg?v=1625856172',
     alt: 'Zebra',
   },
   {
-    id: 10,
+    number: 10,
     src:
       'http://cdn.cnn.com/cnnnext/dam/assets/191008164033-01-fake-fur-leopard.jpg',
     alt: 'Zebra',
   },
   {
-    id: 11,
+    number: 11,
     src:
       'https://inhabitat.com/wp-content/blogs.dir/1/files/2010/07/recycled-bear-skin-rug-6.jpg',
     alt: 'Zebra',
