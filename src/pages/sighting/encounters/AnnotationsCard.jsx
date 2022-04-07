c24ca395d0fa60bf213ac31f522998f0dbfa24f3
Ben Scheiner <brmscheiner@gmail.com>
2022-04-07 15:37:52 -0700

    Remove unnecessary container div

--- src/pages/sighting/encounters/AnnotationsCard.jsx
+++ src/pages/sighting/encounters/AnnotationsCard.jsx
@@ -61,56 +61,54 @@ export default function AnnotationsCard({
         sightingData={sightingData}
       />
       {annotationReferences.length > 0 ? (
-        <div style={{ margin: '8px 0' }}>
-          <Grid container spacing={2}>
-            {annotationReferences.map(annotationReference => {
-              const matchingAsset = assets.find(
-                asset =>
-                  asset?.guid === annotationReference?.asset_guid,
-              );
-              const matchingAssetAnnotations = get(
-                matchingAsset,
-                'annotations',
-                [],
-              );
-              const matchingAnnotation = matchingAssetAnnotations.find(
-                annotation =>
-                  annotation?.guid === annotationReference?.guid,
-              );
-              return (
-                <Grid
-                  key={matchingAnnotation?.guid}
-                  item
-                  style={{ position: 'relative' }}
+        <Grid style={{ margin: '8px 0' }} container spacing={2}>
+          {annotationReferences.map(annotationReference => {
+            const matchingAsset = assets.find(
+              asset =>
+                asset?.guid === annotationReference?.asset_guid,
+            );
+            const matchingAssetAnnotations = get(
+              matchingAsset,
+              'annotations',
+              [],
+            );
+            const matchingAnnotation = matchingAssetAnnotations.find(
+              annotation =>
+                annotation?.guid === annotationReference?.guid,
+            );
+            return (
+              <Grid
+                key={matchingAnnotation?.guid}
+                item
+                style={{ position: 'relative' }}
+              >
+                <AnnotatedPhotograph
+                  assetMetadata={matchingAsset}
+                  annotations={[matchingAnnotation]}
+                  onClick={Function.prototype}
+                  width={248}
+                />
+                <IconButton
+                  onClick={e =>
+                    setAnchorInfo({
+                      element: e.currentTarget,
+                      annotation: matchingAnnotation,
+                    })
+                  }
+                  style={{
+                    position: 'absolute',
+                    top: 12,
+                    right: 12,
+                    color: theme.palette.common.white,
+                  }}
+                  className={classes.photoIcon}
                 >
-                  <AnnotatedPhotograph
-                    assetMetadata={matchingAsset}
-                    annotations={[matchingAnnotation]}
-                    onClick={Function.prototype}
-                    width={248}
-                  />
-                  <IconButton
-                    onClick={e =>
-                      setAnchorInfo({
-                        element: e.currentTarget,
-                        annotation: matchingAnnotation,
-                      })
-                    }
-                    style={{
-                      position: 'absolute',
-                      top: 12,
-                      right: 12,
-                      color: theme.palette.common.white,
-                    }}
-                    className={classes.photoIcon}
-                  >
-                    <MoreIcon />
-                  </IconButton>
-                </Grid>
-              );
-            })}
-          </Grid>
-        </div>
+                  <MoreIcon />
+                </IconButton>
+              </Grid>
+            );
+          })}
+        </Grid>
       ) : (
         <Text
           style={{ marginTop: 4 }}
