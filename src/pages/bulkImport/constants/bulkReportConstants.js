9ebd91147ce86102e6c37f72e2c4a0d71030a3a9
Em <emily@wildme.org>
2022-04-28 10:50:23 -0700

    Use id instead of name for bulk custom fields, cleanup bulk field breakdown
    - Removes redundant bulk field schema from bulkReportConstants

--- src/pages/bulkImport/constants/bulkReportConstants.js
+++ src/pages/bulkImport/constants/bulkReportConstants.js
@@ -41,19 +41,6 @@ export const sightingOmitList = [
 /* Lat and lng are treated as two separate columns here */
 export const encounterOmitList = ['gps', 'specifiedTime'];
 
-export const bulkFieldSchemas = [
-  { name: 'decimalLatitude', labelId: 'DECIMAL_LATITUDE' },
-  { name: 'decimalLongitude', labelId: 'DECIMAL_LONGITUDE' },
-  { name: 'firstName', labelId: 'INDIVIDUAL_NAME' },
-  { name: 'locationId', labelId: 'REGION' },
-  { name: 'timeSpecificity', labelId: 'SIGHTING_TIME_SPECIFICITY' },
-  { name: 'sightingId', labelId: 'SIGHTING_ID' },
-  { name: 'timeYear', labelId: 'TIME_YEAR' },
-  { name: 'timeMonth', labelId: 'TIME_MONTH' },
-  { name: 'timeDay', labelId: 'TIME_DAY' },
-  { name: 'timeHour', labelId: 'TIME_HOUR' },
-  { name: 'timeMinutes', labelId: 'TIME_MINUTES' },
-  { name: 'timeSeconds', labelId: 'TIME_SECONDS' },
-  { name: 'utcOffset', labelId: 'TIMEZONE' },
-  { name: 'assetReferences', labelId: 'ASSETS' },
-];
+export function deriveCustomFieldPrefix(categoryType) {
+  return `custom-${categoryType}-`;
+}
