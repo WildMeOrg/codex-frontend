8c0aa4eebd361d55a9015d73fa199b2a07878471
Em <emily@wildme.org>
2022-05-15 11:33:08 -0700

    Send delete request on uppy file removal

--- src/hooks/useReportUppyInstance.js
+++ src/hooks/useReportUppyInstance.js
@@ -1,8 +1,26 @@
 import { useState, useEffect, useRef, useMemo } from 'react';
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid, validate as uuidValidate } from 'uuid';
 import Tus from '@uppy/tus';
 import Uppy from '@uppy/core';
 import { get } from 'lodash-es';
+import axios from 'axios';
+
+function parseFileGuidFromUploadUrl(uploadUrl) {
+  if (!uploadUrl) throw new Error('Missing upload URL');
+
+  const potentialResourceGuid = uploadUrl
+    .split('/')
+    .filter(token => token)
+    .pop();
+
+  if (!uuidValidate(potentialResourceGuid)) {
+    throw new Error(
+      `Upload URL, ${uploadUrl}, has an invalid resource GUID, "${potentialResourceGuid}".`,
+    );
+  }
+
+  return potentialResourceGuid;
+}
 
 export default function useReportUppyInstance(reportType) {
   const [uppy, setUppy] = useState(null);
@@ -45,8 +63,29 @@ export default function useReportUppyInstance(reportType) {
       setFiles([...fileRef.current, ...assetReferences]);
     });
 
-    uppyInstance.on('file-removed', (file, reason) => {
+    uppyInstance.on('file-removed', async (file, reason) => {
       if (reason === 'removed-by-user') {
+        const uploadUrl = file?.response?.uploadURL;
+        try {
+          const fileGuid = parseFileGuidFromUploadUrl(uploadUrl);
+          await axios.delete(
+            `${__houston_url__}/api/v1/tus/${fileGuid}`,
+            {
+              headers: {
+                'x-tus-transaction-id': assetSubmissionId,
+              },
+            },
+          );
+        } catch (error) {
+          let errorMessage = `${error.name || 'Error'} deleting ${
+            file.name
+          }.`;
+
+          if (error.message) errorMessage += ` ${error.message}`;
+
+          console.error(errorMessage);
+        }
+
         const newFiles = fileRef.current.filter(
           f => f.path !== file.name,
         );
