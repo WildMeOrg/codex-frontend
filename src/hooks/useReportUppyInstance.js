b11fa5c8f3398257ff28413ed3229e50983e9f66
Em <emily@wildme.org>
2022-05-15 11:44:30 -0700

    Add missing dependencies to useReportUppyInstance useEffect

--- src/hooks/useReportUppyInstance.js
+++ src/hooks/useReportUppyInstance.js
@@ -33,71 +33,74 @@ export default function useReportUppyInstance(reportType) {
   const fileRef = useRef([]);
   fileRef.current = files;
 
-  useEffect(() => {
-    const uppyInstance = new Uppy({
-      meta: { type: reportType },
-      restrictions: {
-        allowedFileTypes: ['.jpg', '.jpeg', '.png'],
-      },
-      autoProceed: true,
-    });
+  useEffect(
+    () => {
+      const uppyInstance = new Uppy({
+        meta: { type: reportType },
+        restrictions: {
+          allowedFileTypes: ['.jpg', '.jpeg', '.png'],
+        },
+        autoProceed: true,
+      });
 
-    uppyInstance.use(Tus, {
-      endpoint: `${__houston_url__}/api/v1/tus`,
-      headers: {
-        'x-tus-transaction-id': assetSubmissionId,
-      },
-    });
+      uppyInstance.use(Tus, {
+        endpoint: `${__houston_url__}/api/v1/tus`,
+        headers: {
+          'x-tus-transaction-id': assetSubmissionId,
+        },
+      });
 
-    uppyInstance.on('upload', () => setUploadInProgress(true));
+      uppyInstance.on('upload', () => setUploadInProgress(true));
 
-    uppyInstance.on('complete', uppyState => {
-      const uploadObjects = get(uppyState, 'successful', []);
-      const assetReferences = uploadObjects.map(o => ({
-        path: o.name,
-        transactionId: assetSubmissionId,
-      }));
+      uppyInstance.on('complete', uppyState => {
+        const uploadObjects = get(uppyState, 'successful', []);
+        const assetReferences = uploadObjects.map(o => ({
+          path: o.name,
+          transactionId: assetSubmissionId,
+        }));
 
-      setUploadInProgress(false);
-      setFiles([...fileRef.current, ...assetReferences]);
-    });
+        setUploadInProgress(false);
+        setFiles([...fileRef.current, ...assetReferences]);
+      });
 
-    uppyInstance.on('file-removed', (file, reason) => {
-      if (reason === 'removed-by-user') {
-        const uploadUrl = file?.response?.uploadURL;
-        const fileGuid = parseFileGuidFromUploadUrl(uploadUrl);
-        if (fileGuid) {
-          axios
-            .delete(`${__houston_url__}/api/v1/tus/${fileGuid}`, {
-              headers: {
-                'x-tus-transaction-id': assetSubmissionId,
-              },
-            })
-            .catch(error => {
-              uppyInstance.info(
-                {
-                  message: `Error deleting ${file.name}`,
-                  details: error.message,
+      uppyInstance.on('file-removed', (file, reason) => {
+        if (reason === 'removed-by-user') {
+          const uploadUrl = file?.response?.uploadURL;
+          const fileGuid = parseFileGuidFromUploadUrl(uploadUrl);
+          if (fileGuid) {
+            axios
+              .delete(`${__houston_url__}/api/v1/tus/${fileGuid}`, {
+                headers: {
+                  'x-tus-transaction-id': assetSubmissionId,
                 },
-                'error',
-                7000,
-              );
-            });
+              })
+              .catch(error => {
+                uppyInstance.info(
+                  {
+                    message: `Error deleting ${file.name}`,
+                    details: error.message,
+                  },
+                  'error',
+                  7000,
+                );
+              });
+          }
+
+          const newFiles = fileRef.current.filter(
+            f => f.path !== file.name,
+          );
+          setFiles(newFiles);
         }
+      });
 
-        const newFiles = fileRef.current.filter(
-          f => f.path !== file.name,
-        );
-        setFiles(newFiles);
-      }
-    });
+      setUppy(uppyInstance);
 
-    setUppy(uppyInstance);
-
-    return () => {
-      if (uppyInstance) uppyInstance.close();
-    };
-  }, []);
+      return () => {
+        if (uppyInstance) uppyInstance.close();
+      };
+    },
+    [reportType, assetSubmissionId],
+  );
 
   return {
     uppy,
