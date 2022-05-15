130bf8dad6a6ccec3381fdf6e0776410bb29443d
Em <emily@wildme.org>
2022-05-15 11:33:08 -0700

    Send delete request on uppy file removal

--- src/hooks/useReportUppyInstance.js
+++ src/hooks/useReportUppyInstance.js
@@ -1,8 +1,26 @@
 import { useState, useEffect, useRef, useMemo } from 'react';
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid, validate as uuidValidate } from 'uuid';
 import Tus from '@uppy/tus';
 import Uppy from '@uppy/core';
 import { get } from 'lodash-es';
+import axios from 'axios';
+
+function parseFileGuidFromUploadUrl(uploadUrl) {
+  if (!uploadUrl) return '';
+
+  const potentialResourceGuid = uploadUrl
+    .split('/')
+    .filter(token => token)
+    .pop();
+
+  if (!uuidValidate(potentialResourceGuid)) {
+    console.error(
+      `URL ${uploadUrl} with invalid resource GUID ${potentialResourceGuid}. The resource cannot be deleted.`,
+    );
+    return '';
+  }
+  return potentialResourceGuid;
+}
 
 export default function useReportUppyInstance(reportType) {
   const [uppy, setUppy] = useState(null);
@@ -46,6 +64,27 @@ export default function useReportUppyInstance(reportType) {
 
     uppyInstance.on('file-removed', (file, reason) => {
       if (reason === 'removed-by-user') {
+        const uploadUrl = file?.response?.uploadURL;
+        const fileGuid = parseFileGuidFromUploadUrl(uploadUrl);
+        if (fileGuid) {
+          axios
+            .delete(`${__houston_url__}/api/v1/tus/${fileGuid}`, {
+              headers: {
+                'x-tus-transaction-id': assetSubmissionId,
+              },
+            })
+            .catch(error => {
+              uppyInstance.info(
+                {
+                  message: `Error deleting ${file.name}`,
+                  details: error.message,
+                },
+                'error',
+                7000,
+              );
+            });
+        }
+
         const newFiles = fileRef.current.filter(
           f => f.path !== file.name,
         );
