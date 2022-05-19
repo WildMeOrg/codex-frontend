6001661c1bb6a6045e03b27cec266ef7b8438879
Mark <mark.aaron.fisher@gmail.com>
2022-05-19 13:50:34 -0700

    DEX-1034 add more graceful error handling in MultiSelectViewer, and also troubleshoot that

--- src/components/fields/view/MultiSelectViewer.jsx
+++ src/components/fields/view/MultiSelectViewer.jsx
@@ -10,9 +10,13 @@ export default function MultiSelectViewer({
   defaultLabel = 'Option label not found',
 }) {
   const { choices } = schema;
+  console.log('deleteMe choices in MultiSelectViewer are:');
+  console.log(choices);
+  console.log('deleteMe value in MultiSelectViewer is: ');
+  console.log(value);
 
-  const selectedOptions = choices.filter(choice =>
-    value.find(v => choice[lookupKey] === v),
+  const selectedOptions = choices?.filter(choice =>
+    value?.find(v => choice[lookupKey] === v),
   );
 
   const selectedOptionLabels = selectedOptions.map(o =>
