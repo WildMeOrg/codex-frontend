503a793fc8397746f97b1f5b033997a1df12f8bb
Mark <mark.aaron.fisher@gmail.com>
2022-05-19 15:42:08 -0700

    DEX-1034 split comma separated things in multiselect viewer

--- src/components/fields/view/MultiSelectViewer.jsx
+++ src/components/fields/view/MultiSelectViewer.jsx
@@ -10,15 +10,15 @@ export default function MultiSelectViewer({
   defaultLabel = 'Option label not found',
 }) {
   const { choices } = schema;
-  console.log('deleteMe choices in MultiSelectViewer are:');
-  console.log(choices);
-  console.log('deleteMe value in MultiSelectViewer is: ');
-  console.log(value);
+  const splitValues =
+    typeof value === 'string' ? value.split(',') : value;
+  // console.log('deleteMe choices in MultiSelectViewer are:');
+  // console.log(choices);
+  // console.log('deleteMe value in MultiSelectViewer is: ');
+  // console.log(value);
 
   const selectedOptions = choices?.filter(choice =>
-    typeof value === 'string'
-      ? choice[lookupKey] === value
-      : value?.find(v => choice[lookupKey] === v),
+    splitValues?.find(v => choice[lookupKey] === v),
   );
 
   const selectedOptionLabels = selectedOptions.map(o =>
