3cc65fcd14eeec52ba8203bdf9ceeb4146d1f54f
Mark <mark.aaron.fisher@gmail.com>
2022-06-07 15:56:50 -0700

    DEX-1108 how about now

--- src/components/EditUserMetadata.jsx
+++ src/components/EditUserMetadata.jsx
@@ -51,14 +51,14 @@ export default function EditUserMetadata({
     () => {
       console.log('deleteMe metadata is: ');
       console.log(metadata);
-      // const safeMetadata = filter(
-      //   metadata,
-      //   datum => Object.keys(datum) > 1,
-      // );
-      // console.log('deleteMe safeMetadata is: ');
-      // console.log(safeMetadata);
-      // setFieldValues(getInitialFormValues(safeMetadata));
-      setFieldValues(getInitialFormValues(metadata));
+      const safeMetadata = filter(
+        metadata,
+        datum => Object.keys(datum).length > 1,
+      );
+      console.log('deleteMe safeMetadata is: ');
+      console.log(safeMetadata);
+      setFieldValues(getInitialFormValues(safeMetadata));
+      // setFieldValues(getInitialFormValues(metadata));
     },
     [metadata],
   );
