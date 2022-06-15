762d5024581c074ab9bba5ef26d6093eb416392e
Ben Scheiner <brmscheiner@gmail.com>
2022-06-15 14:50:06 -0700

    Remove console log

--- src/pages/auth/EmailVerified.jsx
+++ src/pages/auth/EmailVerified.jsx
@@ -17,8 +17,6 @@ export default function EmailVerified() {
   const siteName = get(siteSettings, ['site.name', 'value'], '');
   useDocumentTitle('EMAIL_VERIFIED');
 
-  console.log(siteSettings);
-
   return (
     <MainColumn
       style={{
