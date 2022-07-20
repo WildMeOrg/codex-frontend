3d69fb8ee3a93645c6d9c4e510a1052dd393b6f5
Em <emily@wildme.org>
2022-07-19 17:17:17 -0700

    Add progress cards to AssetGroup

--- src/pages/assetGroup/AssetGroup.jsx
+++ src/pages/assetGroup/AssetGroup.jsx
@@ -4,6 +4,7 @@ import { useIntl } from 'react-intl';
 import { get } from 'lodash-es';
 import { useQueryClient } from 'react-query';
 
+import Grid from '@material-ui/core/Grid';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 import errorTypes from '../../constants/errorTypes';
@@ -22,9 +23,20 @@ import MoreMenu from '../../components/MoreMenu';
 import ConfirmDelete from '../../components/ConfirmDelete';
 import EntityHeader from '../../components/EntityHeader';
 import CustomAlert from '../../components/Alert';
+import QueueCard from '../../components/progress/QueueCard';
+import ProgressCard from '../../components/progress/ProgressCard';
+import StartDateCard from '../../components/progress/StartDateCard';
+import TimeRemainingCard from '../../components/progress/TimeRemainingCard';
 import AGSTable from './AGSTable';
 
 const POLLING_INTERVAL = 5000; // 5 seconds
+
+const cardStyle = {
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
 
 function isProgressSettled(pipelineStatus) {
   const { skipped, failed, complete } = pipelineStatus || {};
@@ -98,7 +110,14 @@ export default function AssetGroup() {
     'pipeline_status.preparation',
     {},
   );
-  const isPreparationFailed = pipelineStatusPreparation.failed;
+
+  const {
+    failed: isPreparationFailed,
+    start: preparationStart,
+    ahead: preparationAhead,
+    eta: preparationEta,
+    progress: preparationProgress,
+  } = pipelineStatusPreparation;
 
   const showPreparationInProgressAlert = !(
     isPreparationFailed ||
@@ -178,7 +197,34 @@ export default function AssetGroup() {
               borderTopLeftRadius: 0,
               borderTopRightRadius: 0,
             }}
-          />
+          >
+            <Grid container spacing={1} style={{ paddingTop: 8 }}>
+              <Grid item xs={6}>
+                <QueueCard
+                  ahead={preparationAhead}
+                  style={cardStyle}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <StartDateCard
+                  startDate={preparationStart}
+                  style={cardStyle}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <ProgressCard
+                  progress={preparationProgress}
+                  style={cardStyle}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TimeRemainingCard
+                  timeRemaining={preparationEta}
+                  style={cardStyle}
+                />
+              </Grid>
+            </Grid>
+          </CustomAlert>
         </>
       )}
       <AGSTable
