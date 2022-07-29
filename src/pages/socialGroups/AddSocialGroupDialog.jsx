7f0c3d031c16f987af2acc740f7270a32dbf6cdc
Ben Scheiner <brmscheiner@gmail.com>
2022-07-29 13:47:23 -0700

    Loading and error states in AddSocialGroupDialog

--- src/pages/socialGroups/AddSocialGroupDialog.jsx
+++ src/pages/socialGroups/AddSocialGroupDialog.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
 
 import usePostSocialGroup from '../../models/socialGroups/usePostSocialGroup';
+import CustomAlert from '../../components/Alert';
 import StandardDialog from '../../components/StandardDialog';
 import Button from '../../components/Button';
 import TextInput from '../../components/inputs/TextInput';
 
 export default function AddSocialGroupDialog({ open, onClose }) {
-  const { mutate: postSocialGroup, loading } = usePostSocialGroup();
+  const {
+    mutate: postSocialGroup,
+    loading,
+    error,
+    clearError,
+  } = usePostSocialGroup();
 
   const [name, setName] = useState('');
 
-  const onCloseDialog = () => {
+  const onCloseDialog = useCallback(() => {
     setName('');
+    if (error) clearError();
     onClose();
-  };
+  }, [error]);
 
   return (
     <StandardDialog
@@ -32,18 +39,41 @@ export default function AddSocialGroupDialog({ open, onClose }) {
           variant="outlined"
         />
       </DialogContent>
-      <DialogActions>
-        <Button display="basic" onClick={onCloseDialog} id="CLOSE" />
-        <Button
-          display="primary"
-          disabled={name === ''}
-          loading={loading}
-          onClick={async () => {
-            const result = await postSocialGroup({ name });
-            if (result?.status === 200) onCloseDialog();
-          }}
-          id="ADD"
-        />
+      <DialogActions
+        style={{
+          padding: '0px 24px 24px 24px',
+          display: 'flex',
+          alignItems: 'flex-end',
+          flexDirection: 'column',
+        }}
+      >
+        {error && (
+          <CustomAlert
+            style={{ margin: '20px 0', width: '100%' }}
+            severity="error"
+            titleId="SERVER_ERROR"
+          >
+            {error}
+          </CustomAlert>
+        )}
+        <div>
+          <Button
+            style={{ marginRight: 4 }}
+            display="basic"
+            onClick={onCloseDialog}
+            id="CLOSE"
+          />
+          <Button
+            display="primary"
+            disabled={name === ''}
+            loading={loading}
+            onClick={async () => {
+              const result = await postSocialGroup({ name });
+              if (result?.status === 200) onCloseDialog();
+            }}
+            id="ADD"
+          />
+        </div>
       </DialogActions>
     </StandardDialog>
   );
