ed308773058e706acc8125913989270cf873cef3
Ben Scheiner <brmscheiner@gmail.com>
2022-08-12 12:37:13 -0700

    Fix script

--- scripts/printUnusedKeys.js
+++ scripts/printUnusedKeys.js
@@ -2,25 +2,44 @@ const { find } = require('find-in-files');
 const englishTranslations = require('../locale/en.json');
 
 const translationKeys = Object.keys(englishTranslations);
-let count = 0;
+let unusedCount = 0;
+
+function getLinesWithMatch(lines, substring) {
+  return lines.filter(line => line.includes(substring));
+}
 
 async function printUnusedKeys() {
   /* eslint-disable */
   for await (const translationKey of translationKeys) {
-    const results = await find(`"${translationKey}"`, './src');
-    const matchingFiles = Object.keys(results);
-    if (matchingFiles.length === 0) {
-      count += 1;
+    const results = await find(translationKey, './src');
+    const matches = Object.values(results);
+    const trueMatches = matches.filter(match => {
+      const matchingLines = match?.line || [];
+      const linesWithSingleQuoteMatch = getLinesWithMatch(
+        matchingLines,
+        `"${translationKey}"`,
+      );
+      const linesWithDoubleQuoteMatch = getLinesWithMatch(
+        matchingLines,
+        `\'${translationKey}\'`,
+      );
+      return (
+        linesWithSingleQuoteMatch.length > 0 ||
+        linesWithDoubleQuoteMatch.length > 0
+      );
+    });
+    if (trueMatches.length === 0) {
+      unusedCount += 1;
       console.log(`"${translationKey}" may be unused.`);
     }
   }
   /* eslint-enable */
+  console.log(
+    `Script finished. ${unusedCount} potentially unused translation keys found.`,
+  );
 }
 
 console.log(
   'FYI, this script takes forever to run! Recommendation is to leave it running overnight or in the background.',
 );
 printUnusedKeys();
-console.log(
-  `Script finished. ${count} potentially unused translation keys found.`,
-);
