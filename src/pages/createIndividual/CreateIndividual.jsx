f3c50fde18a196a14520fa119fe4615b968bfcdc
erinz <ubuntu@ErinZhang-Server-3.1mo1zcdn0wmehmknn4mbmgwvmg.ex.internal.cloudapp.net>
2023-04-25 13:44:34 +0000

    cdx-37

--- src/pages/createIndividual/CreateIndividual.jsx
+++ src/pages/createIndividual/CreateIndividual.jsx
@@ -181,6 +181,7 @@ export default function CreateIndividual() {
                   {formError}
                 </Text>
               ))}
+              {createIndividualError}
             </Alert>
           )}
           <Button
