6e71bd1c16164b061b2157e7871d8d9fafc196d4
erinz <ubuntu@ErinZhang-Server-3.1mo1zcdn0wmehmknn4mbmgwvmg.ex.internal.cloudapp.net>
2023-06-01 23:15:01 +0000

    deal with toxonomy is null

--- src/pages/merge/ResolutionSelector.jsx
+++ src/pages/merge/ResolutionSelector.jsx
@@ -18,9 +18,66 @@ export default function ResolutionSelector({
   value,
   onChange,
   fieldType,
-  individualData,
+  // individualData,
 }) {
+
+  const individualData = {"123233": 
+  {
+      
+      "names": [
+          {
+              "context": "FirstName",
+              "elasticsearchable": false,
+              "indexed": "2023-05-30T19:16:29.193943+00:00",
+              "guid": "b5468eb7-2f01-432f-ad59-a5cb644e6f7122222",
+              "creator": {
+                  "full_name": "Erin",
+                  "profile_fileupload": null,
+                  "guid": "65362056-c22a-4d8c-986c-30954ad10bc511111"
+              },
+              "value": null,
+              "preferring_users": []
+          }
+      ],
+      "featuredAssetGuid": "2868250a-0be6-4fb4-9720-7e837b901893",
+      "sex": "male",    
+      "guid": "4050f325-caa5-4f60-bcd1-7700b65ee245",
+      "hasView": true,
+      "updated": "2023-06-01T17:12:58.128646+00:00",
+      "taxonomy": null,    
+      "created": "2023-05-22T19:30:09.771763+00:00"
+  }
+,
+
+"78789789": 
+  {
+      
+      "names": [
+          {
+              "context": "FirstName",
+              "elasticsearchable": false,
+              "indexed": "2023-05-30T19:16:29.193943+00:00",
+              "guid": "b5468eb7-2f01-432f-ad59-a5cb644e6f71",
+              "creator": {
+                  "full_name": "Erin",
+                  "profile_fileupload": null,
+                  "guid": "65362056-c22a-4d8c-986c-30954ad10bc5"
+              },
+              "value": "victoria",
+              "preferring_users": []
+          }
+      ],
+      "featuredAssetGuid": "2868250a-0be6-4fb4-9720-7e837b901893",
+      "sex": "male",    
+      "guid": "4050f325-caa5-4f60-bcd1-7700b65ee245",
+      "hasView": true,
+      "updated": "2023-06-01T17:12:58.128646+00:00",
+      "taxonomy": '1-2-3-4-5',    
+      "created": "2023-05-22T19:30:09.771763+00:00"
+  
+}}
     
+  console.log("individualData in selector",individualData);
   const { data, loading, error } = useSiteSettings();
   const species = get(data, ['site.species', 'value'], []);
   const speciesOptions = species.map(s => {
@@ -65,7 +122,7 @@ export default function ResolutionSelector({
       getProperty: individualData => individualData?.taxonomy,
       deriveChoices: species => {
         if(species.length === 0) return speciesOptions;
-        return species.map(data => {
+        return species.filter(data => data!== null && data !== undefined).map(data => {
           const speciesLabel = speciesOptions.find(data1 => data1.value === data);
           return ({
             label: speciesLabel ? speciesLabel.label : 'Label not found',
@@ -76,9 +133,12 @@ export default function ResolutionSelector({
     },
   };
   const selectorSchema = propertyMap[fieldType];
+  console.log("individualData",Object.values(individualData));
+  
   const individualProperties = Object.values(individualData).map(
     individual => selectorSchema.getProperty(individual),
   );
+  console.log("individualProperties",individualProperties)
   const choices = useMemo(() => {
     if (individualProperties?.length === 0) return [];
     return selectorSchema.deriveChoices(individualProperties);
