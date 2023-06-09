20f6fe5940ea5341caa9c5a785cf4621336698d9
erinz <ubuntu@ErinZhang-Server-3.1mo1zcdn0wmehmknn4mbmgwvmg.ex.internal.cloudapp.net>
2023-06-09 22:33:11 +0000

    cdx-25

---
+++ src/pages/reportSighting/ReportSightingBreadcrumbs.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { useTheme } from '@material-ui/core/styles';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+
+import Link from './Link';
+import Text from './Text';
+
+export default function ReportSightingBreadcrumbs({
+  currentPageText,
+  currentPageTextId,
+}) {
+  const theme = useTheme();
+
+  const linkStyles = {
+    color: theme.palette.text.secondary,
+  };
+  return (
+    <Breadcrumbs
+      style={{ margin: '8px 0 0 20px' }}
+      aria-label="breadcrumb"
+    >
+      <Link href="/" style={linkStyles}>
+        <Text id="HOME" />
+      </Link>
+      <Link href="/report" style={linkStyles}>
+        <Text id="CONTROL_PANEL" />
+      </Link>
+      <Link href="/report" style={linkStyles}>
+        <Text id="CONTROL_PANEL" />
+      </Link>
+      <Text id={currentPageTextId}>{currentPageText}</Text>
+    </Breadcrumbs>
+  );
+}
