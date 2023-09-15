dfb53b2e65360124274f277eedbe3c4eeffc0adb
tsubramanian <tamilselvan@gmail.com>
2023-09-15 22:44:39 +0530

    Update build.npm.sh

--- scripts/build.npm.sh
+++ scripts/build.npm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-#export NODE_OPTIONS=--openssl-legacy-provider
+export NODE_OPTIONS=--openssl-legacy-provider
 npm install --legacy-peer-deps
 npm run build -- --env=houston=relative
