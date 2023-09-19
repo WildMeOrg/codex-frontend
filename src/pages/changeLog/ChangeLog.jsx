76f88ace9ccdcdb54d1e56407d89ab9f9c777027
erinz <ubuntu@ErinZhang-Server-3.1mo1zcdn0wmehmknn4mbmgwvmg.ex.internal.cloudapp.net>
2023-09-19 17:42:41 +0000

    fix lint error

--- src/pages/changeLog/ChangeLog.jsx
+++ src/pages/changeLog/ChangeLog.jsx
@@ -1,198 +1,183 @@
 import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import useTheme from '@material-ui/core/styles/useTheme';
+import { useIntl, FormattedMessage } from 'react-intl';
+import SearchIcon from '@material-ui/icons/Search';
+import TextField from '@material-ui/core/TextField';
 import useDocumentTitle from '../../hooks/useDocumentTitle';
 import MainColumn from '../../components/MainColumn';
 import Button from '../../components/Button';
 import SettingsBreadcrumbs from '../../components/SettingsBreadcrumbs';
 import Text from '../../components/Text';
 import Link from '../../components/Link';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import useTheme from '@material-ui/core/styles/useTheme';
-import { useIntl, FormattedMessage } from 'react-intl';
 import DataDisplay from '../../components/dataDisplays/DataDisplay';
-import SearchIcon from '@material-ui/icons/Search';
 import useFilterAuditLogs from '../../models/auditLogs/useFilterAuditLogs';
 import Paginator from '../../components/dataDisplays/Paginator';
-import TextField from '@material-ui/core/TextField';
 import SadScreen from '../../components/SadScreen';
 import LoadingScreen from '../../components/LoadingScreen';
 
-export default function ChangeLog() {
-    
-  useDocumentTitle('CHANGE_LOG');
-  const theme = useTheme();  
-  const intl = useIntl();
-
-  const [inputValue, setInputValue] = useState(''); 
-
-  
-  const rowsPerPage = 100;  
-  const [searchParams, setSearchParams] = useState({
-    limit: rowsPerPage,
-    offset: 0,
-    sort: 'created',
-    reverse: true,
-  });  
-
-  const [ formFilters, setFormFilters ] = useState(
-    {
-      "bool" : {
-          "filter" : [],
-      }
-    }
-  ); 
-
-  const buildFilters = () => {
-    if(inputValue.trim() !== '') {
-      setFormFilters(
-        {
-          "bool" : {
-             "filter" : [
-                {
-                       "match": {
-                               "item_guid": inputValue.trim()
-                       }
-                }
-             ]
-          }
-       }
-    )
-    } else {
-      setFormFilters(
-        {
-          "bool" : {
-             "filter" : []
-          }
-       }
-    )
-    } 
-  }
-
-  const { data : searchedData, loading } = useFilterAuditLogs({
-    queries: formFilters,
-    params: searchParams,
-  });
-
-  const buildAndSortTableRows = (data) => {
-    return data?.map((row) => {
-      return {
-        id: row.guid,
-        time: new Date(row.created).toLocaleString(),
-        message: `MODULE NAME: ${row.module_name}, ITEM GUID: ${row.item_guid}, AUDIT TYPE:  ${row.audit_type},  DETAILS: ${row.message}`,
-      }
-    })?.sort((a, b) => new Date(b.time) - new Date(a.time));
-  }  
-
-  
-const tableColumns = [
+const buildTableColumns = intl => [
   {
     name: 'time',
     label: intl.formatMessage({ id: 'TIME_CHANGE_OCCURRED' }),
     options: {
       customBodyRender: labelId => (
-        <FormattedMessage id={labelId} defaultMessage={labelId}/>
+        <FormattedMessage id={labelId} defaultMessage={labelId} />
       ),
     },
   },
   {
     name: 'message',
     label: intl.formatMessage({ id: 'MESSAGE' }),
-  //   options: { cellRenderer: cellRendererTypes.capitalizedString },
-  },    
-]; 
-  const searchedTableRows = buildAndSortTableRows(searchedData?.results);  
-    
-  if(loading) return <LoadingScreen />  
-  if(searchedData?.statusCode === 403) return <SadScreen
-    statusCode={searchedData?.statusCode}
-/>
+    //   options: { cellRenderer: cellRendererTypes.capitalizedString },
+  },
+];
+
+export default function ChangeLog() {
+  useDocumentTitle('CHANGE_LOG');
+  const theme = useTheme();
+  const intl = useIntl();
+
+  const tableColumns = buildTableColumns(intl);
+  const [inputValue, setInputValue] = useState('');
+
+  const rowsPerPage = 100;
+  const [searchParams, setSearchParams] = useState({
+    limit: rowsPerPage,
+    offset: 0,
+    sort: 'created',
+    reverse: true,
+  });
+
+  const [formFilters, setFormFilters] = useState({
+    bool: {
+      filter: [],
+    },
+  });
+
+  const buildFilters = () => {
+    if (inputValue.trim() !== '') {
+      setFormFilters({
+        bool: {
+          filter: [
+            {
+              match: {
+                item_guid: inputValue.trim(),
+              },
+            },
+          ],
+        },
+      });
+    } else {
+      setFormFilters({
+        bool: {
+          filter: [],
+        },
+      });
+    }
+  };
+
+  const { data: searchedData, loading } = useFilterAuditLogs({
+    queries: formFilters,
+    params: searchParams,
+  });
+
+  const buildAndSortTableRows = data =>
+    data
+      ?.map(row => ({
+        id: row.guid,
+        time: new Date(row.created).toLocaleString(),
+        message: `MODULE NAME: ${row.module_name}, ITEM GUID: ${row.item_guid}, AUDIT TYPE:  ${row.audit_type},  DETAILS: ${row.message}`,
+      }))
+      ?.sort((a, b) => new Date(b.time) - new Date(a.time));
+
+  const searchedTableRows = buildAndSortTableRows(
+    searchedData?.results,
+  );
+
+  if (loading) return <LoadingScreen />;
+  if (searchedData?.statusCode === 403)
+    return <SadScreen statusCode={searchedData?.statusCode} />;
 
   return (
     <MainColumn>
-        <Link href="/settings"                   
-            style={{
-            display:'flex',
-            alignItems:'center',
-            color: theme.palette.text.primary,
-            fontSize: 22,
-            fontWeight: 'bold',
-            textDecoration: 'none',
-            marginTop: 80,
-            }}
-            >  
-            <ArrowBackIcon />
-            {<FormattedMessage id={'CHANGE_LOG'}/>}  
-        </Link> 
+      <Link
+        href="/settings"
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          color: theme.palette.text.primary,
+          fontSize: 22,
+          fontWeight: 'bold',
+          textDecoration: 'none',
+          marginTop: 80,
+        }}
+      >
+        <ArrowBackIcon />
+        <FormattedMessage id="CHANGE_LOG" />
+      </Link>
 
       <SettingsBreadcrumbs currentPageTextId="CHANGE_LOG" />
-      
-      <Grid item>   
+
+      <Grid item>
         <div
-            style={{
+          style={{
             display: 'flex',
             justifyContent: 'space-between',
             alignItems: 'center',
             marginTop: 12,
-            }}
-        >  
-            <Text
-                variant="h5"
-                component="h5"
-                id="CHANGE_LOG"
-            />
+          }}
+        >
+          <Text variant="h5" component="h5" id="CHANGE_LOG" />
 
-            <div
-              style={{
-                marginTop: 20,
-                display: 'flex',
-                alignItems: 'flex-end',
-                marginBottom: 12, 
+          <div
+            style={{
+              marginTop: 20,
+              display: 'flex',
+              alignItems: 'flex-end',
+              marginBottom: 12,
+            }}
+          >
+            <Button
+              startIcon={<SearchIcon />}
+              loading={loading}
+              onClick={() => buildFilters(inputValue)}
+              style={{ height: 30, border: 'none' }}
+            />
+            <TextField
+              id="entity-guid-input"
+              label="ITEM GUID"
+              value={inputValue}
+              onChange={e => setInputValue(e.target.value)}
+              onKeyDown={event => {
+                if (event.key === 'Enter') {
+                  buildFilters(inputValue);
+                }
               }}
-            >
-              <Button
-                startIcon={<SearchIcon />}
-                loading={loading}
-                onClick = { () => buildFilters(inputValue)}
-                style={{ height: 30, border: 'none' }}
-              >
-                
-              </Button>
-              <TextField
-                id="entity-guid-input"
-                label="ITEM GUID"
-                value={inputValue}
-                onChange={e => setInputValue(e.target.value)}
-                onKeyDown={event => {
-                  if (event.key === 'Enter') {
-                    buildFilters(inputValue)
-                  }
-                }}
-                style={{ width: 420 }}
-              />
-              
-            </div>
-            
+              style={{ width: 420 }}
+            />
+          </div>
         </div>
-      <DataDisplay
-        style={{ marginTop: 8 }}
-        noTitleBar
-        cellStyles = {{overflow: 'wrap', maxWidth: 300}}
-        variant="secondary"
-        columns={tableColumns}
-        data={searchedTableRows}
-        tableContainerStyles={{ maxHeight: 1000 }}
-        searchParams={searchParams}
-        setSearchParams={setSearchParams}
-        
-        noResultsTextId="NO_SEARCH_RESULT"
-      />
-      <Paginator
-        searchParams={searchParams}
-        setSearchParams={setSearchParams}
-        count={searchedData?.resultCount}
-      />
-    </Grid>
+        <DataDisplay
+          style={{ marginTop: 8 }}
+          noTitleBar
+          cellStyles={{ overflow: 'wrap', maxWidth: 500 }}
+          variant="secondary"
+          columns={tableColumns}
+          data={searchedTableRows}
+          tableContainerStyles={{ maxHeight: 1000 }}
+          searchParams={searchParams}
+          setSearchParams={setSearchParams}
+          noResultsTextId="NO_SEARCH_RESULT"
+        />
+        <Paginator
+          searchParams={searchParams}
+          setSearchParams={setSearchParams}
+          count={searchedData?.resultCount}
+        />
+      </Grid>
     </MainColumn>
   );
 }
