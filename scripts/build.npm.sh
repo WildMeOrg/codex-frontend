9393878ab976c077f5a1dd57c15bdf86f8c7c000
tsubramanian <tamilselvan@gmail.com>
2023-09-19 07:42:38 +0530

    Update build.npm.sh

--- scripts/build.npm.sh
+++ scripts/build.npm.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export NODE_OPTIONS="--openssl-legacy-provider --max_old_space_size=8192"
+npm remove node_modules 
+npm remove package-lock.json
 npm install -g npm@10.1.0
 npm cache clean --force
 npm install --legacy-peer-deps --maxsockets 1
