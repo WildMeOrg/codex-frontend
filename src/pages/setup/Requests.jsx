b54b4db0bcb17da8b3210af7b073602c0d9acad4
erinz2020 <erinz2020@gmail.com>
2023-10-31 19:53:51 +0000

    lint

--- src/pages/setup/Requests.jsx
+++ src/pages/setup/Requests.jsx
@@ -1,68 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import MainColumn from '../../components/MainColumn';
+import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import { useIntl } from 'react-intl';
+import MainColumn from '../../components/MainColumn';
 import Text from '../../components/Text';
 import DataDisplay from '../../components/dataDisplays/DataDisplay';
 import useGetRequests from '../../models/users/useGetRequests';
-import { useIntl } from 'react-intl';
 import SadScreen from '../../components/SadScreen';
 import LoadingScreen from '../../components/LoadingScreen';
 
 export default function Requests() {
-    const intl = useIntl();
-    const {
-        data,
-        isLoading,
-        error,
-        statusCode
-      } = useGetRequests();
-    console.log("statusCode: ", statusCode);
-      
-    let requests = data || [];
-    requests = requests
-      .map((request) => {
-        request.created = new Date(request.created).toLocaleString(); return request;})
-      .sort((a, b) => new Date(b.created) - new Date(a.created));
+  const intl = useIntl();
+  const { data, isLoading, error, statusCode } = useGetRequests();
 
-    const columns = [
-        {
-          name: 'created',
-          label: intl.formatMessage({ id: 'CREATED' }),
-        },
-        {
-          name: 'name',
-          label: intl.formatMessage({ id: 'NAME' }),
-        },
-        {
-          name: 'email',
-          label: intl.formatMessage({ id: 'EMAIL' }),
-        },
-        {
-          name: 'message',
-          label: intl.formatMessage({ id: 'MESSAGE' }),
-        }        
-      ];
-  if(isLoading) return <LoadingScreen />    
+  let requests = data || [];
+  requests = requests
+    .map(request => {
+      request.created = new Date(request.created).toLocaleString();
+      return request;
+    })
+    .sort((a, b) => new Date(b.created) - new Date(a.created));
 
-  if(error) return <SadScreen
-      statusCode={statusCode}
-/>
+  const columns = [
+    {
+      name: 'created',
+      label: intl.formatMessage({ id: 'CREATED' }),
+    },
+    {
+      name: 'name',
+      label: intl.formatMessage({ id: 'NAME' }),
+    },
+    {
+      name: 'email',
+      label: intl.formatMessage({ id: 'EMAIL' }),
+    },
+    {
+      name: 'message',
+      label: intl.formatMessage({ id: 'MESSAGE' }),
+    },
+  ];
+  if (isLoading) return <LoadingScreen />;
+
+  if (error) return <SadScreen statusCode={statusCode} />;
   return (
-    <MainColumn style={{
-        display: 'flex', 
+    <MainColumn
+      style={{
+        display: 'flex',
         flexDirection: 'column',
-        justifyContent:'center',
+        justifyContent: 'center',
         alignItems: 'center',
-        }}>
-      <Text variant="h4" style={{ marginBottom: 24 }} id="MANAGE_REQUESTS" />
+      }}
+    >
+      <Text
+        variant="h4"
+        style={{ marginBottom: 24 }}
+        id="MANAGE_REQUESTS"
+      />
       <Card>
         <CardContent>
-          <Text id="INVITATION_REQUESTS" style={{ fontWeight: 'bold' }} />
+          <Text
+            id="INVITATION_REQUESTS"
+            style={{ fontWeight: 'bold' }}
+          />
           <DataDisplay
-            idKey={'guid'}
-            tableContainerStyles={{ minWidth:700, maxWidth: 750 }}
-            cellStyles = {{overflow: 'wrap', maxWidth: 300}}
+            idKey="guid"
+            tableContainerStyles={{ minWidth: 700, maxWidth: 750 }}
+            cellStyles={{ overflow: 'wrap', maxWidth: 300 }}
             style={{ marginTop: 12 }}
             noTitleBar
             variant="secondary"
@@ -73,7 +76,6 @@ export default function Requests() {
           />
         </CardContent>
       </Card>
-    </MainColumn> 
-    
+    </MainColumn>
   );
 }
