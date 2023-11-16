5d0cd06bced9d052bde4bb10799356a237038098
erinz2020 <erinz2020@gmail.com>
2023-11-16 15:54:27 +0000

    region component remodel part1

--- src/components/fields/edit/LocationIdEditor.jsx
+++ src/components/fields/edit/LocationIdEditor.jsx
@@ -3,7 +3,7 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import { Autocomplete } from '@material-ui/lab';
 import { TextField } from '@material-ui/core';
 import { get } from 'lodash-es';
-import Text from '../../Text';
+import Radio from '@material-ui/core/Radio';
 import useDescription from '../../../hooks/useDescription';
 import useEditLabel from '../../../hooks/useEditLabel';
 import FormCore from './FormCore';
@@ -35,19 +35,71 @@ export default function LocationIdEditor(props) {
     : null;
   const currentRegion = get(currentRegionArray, [0], null);
 
+  const filterOptions = (options, { inputValue }) => {
+    console.log('options', options);
+    console.log('inputValue', inputValue);
+
+    if (inputValue) {
+      // const filteredOptions = options.filter(option =>
+      //   option.name.toLowerCase().includes(inputValue.toLowerCase())
+      // );
+
+      return options;
+    }
+
+    return options;
+  };
+
   return (
     <FormCore schema={schema} width={width}>
       <Autocomplete
         value={currentRegion}
         options={collapsedChoices}
-        renderOption={option => (
-          <Text
-            style={{ paddingLeft: option.depth * 10 }}
-            value={option.id}
-          >
-            {option.name}
-          </Text>
-        )}
+        disableCloseOnSelect
+        filterOptions={filterOptions}
+        // renderOption={option => (
+        //   <Text
+        //     style={{ paddingLeft: option.depth * 10 }}
+        //     value={option.id}
+        //   >
+        //     {option.name}
+        //   </Text>
+        // )}
+        renderOption={(option, { selected, inputValue }) => {
+          const searchResult = option.name
+            .toLowerCase()
+            .includes(inputValue.toLowerCase());
+
+          return (
+            <>
+              <Radio
+                style={{
+                  paddingLeft: option.depth * 10,
+                  fontSize: 5,
+                }}
+                checked={selected || searchResult}
+                value={option.id}
+                onChange={() => {}}
+                color="primary"
+                inputProps={{ 'aria-labelledby': option.id }}
+              />
+              {searchResult ? (
+                <span
+                  style={{
+                    fontSize: 42,
+                    paddingLeft: option.depth * 10,
+                  }}
+                >
+                  {option.name}
+                </span>
+              ) : (
+                <span style={{ paddingLeft: option.depth * 10 }}>
+                  {option.name}
+                </span>
+              )}
+            </>
+          );
+        }}
         onChange={(_, newValue) => {
           if (multiple) {
             onChange(
@@ -62,12 +114,14 @@ export default function LocationIdEditor(props) {
           option.id ? option.id === get(currentRegion, 'id') : false
         }
         renderInput={params => (
-          <TextField
-            {...params}
-            style={{ width: 280 }}
-            variant="standard"
-            label={editLabel}
-          />
+          <div>
+            <TextField
+              {...params}
+              style={{ width: 280 }}
+              variant="standard"
+              label={editLabel}
+            />
+          </div>
         )}
         multiple={multiple}
         {...rest}
