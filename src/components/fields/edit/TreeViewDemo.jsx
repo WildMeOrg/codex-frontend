beaa54e1cd3e548c319384e4491a901ca16afd93
erinz2020 <erinz2020@gmail.com>
2023-11-27 09:57:45 +0000

    hierachy list

--- src/components/fields/edit/TreeViewDemo.jsx
+++ src/components/fields/edit/TreeViewDemo.jsx
@@ -2,65 +2,12 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TreeView from '@material-ui/lab/TreeView';
 import TreeItem from '@material-ui/lab/TreeItem';
-import TextField from '@material-ui/core/TextField';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Radio from '@material-ui/core/Radio';
 import Paper from '@material-ui/core/Paper';
+import _ from 'lodash-es';
 
-
-const data = 
-
-[
-    {
-        "id": "d6ae1777-b03a-45cd-8cf9-216ed30de263",
-        "name": "only"
-    },
-    {
-        "id": "e955e661-cda0-4115-ab45-fab9b85c32fb",
-        "name": "Asia",
-        "locationID": [
-            {
-                "id": "829c5726-a4fe-4029-baea-ad25cf652579",
-                "name": "ITEM2"
-            },
-            {
-                "id": "e2b3b422-8ca9-45a7-b3e6-fe1d37b761bd",
-                "name": "ITEM1"
-            }
-        ]
-    },
-    {
-        "id": "abea08f3-6855-4dff-a872-4593faddbc35",
-        "name": "Africa",
-        "locationID": [
-            {
-                "id": "5030f593-bbbb-448c-a049-ed7012d55edb",
-                "name": "sub4",
-                "locationID": [
-                    {
-                        "id": "aa5ce705-3a39-4d30-83d3-89e6f200696d",
-                        "name": "sub5"
-                    }
-                ]
-            },
-            {
-                "id": "72a4fa59-b53c-4346-af37-8bae0f8991ff",
-                "name": "sub2",
-                "locationID": [
-                    {
-                        "id": "d8f6ac7d-1eca-40e2-817c-12c6be68c41c",
-                        "name": "sub3"
-                    }
-                ]
-            },
-            {
-                "id": "739452af-d1db-43ad-a57f-89293f39fa15",
-                "name": "sub1"
-            }
-        ]
-    }
-];
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -80,54 +27,46 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-const renderTree = (nodes, searchText) => {
-  if (searchText && !nodes.name.toLowerCase().includes(searchText.toLowerCase())) {
-    return null;
-  }
-  const matchesSearch = searchText && nodes.name.toLowerCase().includes(searchText.toLowerCase());
-
-
-  return (
-    <TreeItem
-      key={nodes.id}
-      nodeId={nodes.id}
-      label={
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <Radio />
-          {nodes.name}
-        </div>
-      }
-    >
-      {Array.isArray(nodes.locationID)
-        ? nodes.locationID.map((node) => renderTree(node, searchText))
-        : null}
-    </TreeItem>
-  );
-};
-
 const TreeViewDemo = (props) => {
   const {    
     onChange,    
+    searchText,
+    showData
   } = props;
   const classes = useStyles();
-  const [searchText, setSearchText] = useState('');
-  const handleSearchChange = (event) => {
-    setSearchText(event.target.value);
-  };
+  const [selected, setSelected] = useState(null);
 
   const handleNodeSelect = (event, nodeId) => {
-    console.log('selected', nodeId);
     onChange(nodeId);
+    setSelected(nodeId);
   };
 
+  const renderItem = (node) => {
+    return (
+      <TreeItem
+        key={node.id}
+        nodeId={node.id}
+        label={
+          <div style={{ 
+            width: '100%', 
+            display: 'flex', 
+            alignItems: 'center',
+            }}>
+            <Radio checked={selected===node.id}/>
+            {node.name}            
+          </div>
+        }
+      >
+        {Array.isArray(node.locationID)
+          ? node.locationID.map((node) => renderItem(node, searchText))
+          : null}
+      </TreeItem>
+    )
+  }
+
+
   return (
-    <Paper style={{maxHeight:200, width: '100%', overflow:"auto"}}>
-      <TextField
-        style={{ width:'100%' }}
-        label="Search"
-        value={searchText}
-        onChange={handleSearchChange}
-      />
+    <Paper style={{maxHeight:1200, width: '100%', overflow:"auto"}}>
       <TreeView
         onNodeSelect={handleNodeSelect}
 
@@ -135,91 +74,13 @@ const TreeViewDemo = (props) => {
         defaultCollapseIcon={<ExpandMoreIcon />}
         defaultExpandIcon={<ChevronRightIcon />}
       >
-        {data.map((node) => renderTree(node, searchText))}
+        {/* {data.map((node) => renderTree(node, searchText))} */}
+        {(_.isNil(showData) || !_.isArray(showData) || _.isEmpty(showData)) 
+          ? <></> 
+          : showData.map(node => renderItem(node))}
       </TreeView>
     </Paper>
   );
 };
-
-// const renderTree = (nodes, searchText, expandedNodes, onNodeToggle) => {
-//   if (searchText && !nodes.name.toLowerCase().includes(searchText.toLowerCase())) {
-//     return null;
-//   }
-//   const matchesSearch = searchText && nodes.name.toLowerCase().includes(searchText.toLowerCase());
-
-//   const isExpanded = expandedNodes.includes(nodes.id);
-
-//   return (
-//     <TreeItem
-//       key={nodes.id}
-//       nodeId={nodes.id}
-//       label={
-//         <div style={{ display: 'flex', alignItems: 'center' }}>
-//           <Radio />
-//           {nodes.name}
-//         </div>
-//       }
-//       onIconClick={() => onNodeToggle(nodes.id)}
-//       onLabelClick={() => onNodeToggle(nodes.id)}
-//       icon={nodes.locationID && nodes.locationID.length > 0 ? (isExpanded ? <ExpandMoreIcon /> : <ChevronRightIcon />) : null}
-//     >
-//       {Array.isArray(nodes.locationID) && matchesSearch && isExpanded
-//         ? nodes.locationID.map((node) => renderTree(node, searchText, expandedNodes, onNodeToggle))
-//         : null}
-//     </TreeItem>
-//   );
-// };
-
-// const TreeViewDemo = (props) => {
-//   // ... (unchanged code)
-
-//   const {    
-//         onChange,    
-//       } = props;
-//       const classes = useStyles();
-//       const [searchText, setSearchText] = useState('');
-//       const handleSearchChange = (event) => {
-//         setSearchText(event.target.value);
-//       };
-    
-//       const handleNodeSelect = (event, nodeId) => {
-//         console.log('selected', nodeId);
-//         onChange(nodeId);
-//       };
-    
-
-//   const [expandedNodes, setExpandedNodes] = useState([]);
-
-//   const handleNodeToggle = (nodeId) => {
-//     setExpandedNodes((prevExpandedNodes) => {
-//       if (prevExpandedNodes.includes(nodeId)) {
-//         return prevExpandedNodes.filter((id) => id !== nodeId);
-//       } else {
-//         return [...prevExpandedNodes, nodeId];
-//       }
-//     });
-//   };
-
-//   return (
-//     <Paper style={{maxHeight:200, width: '100%', overflow:"auto"}}>
-//       {/* ... (unchanged code) */}
-//       <TextField
-//         style={{ width:'100%' }}
-//         label="Search"
-//         value={searchText}
-//         onChange={handleSearchChange}
-//       />
-//       <TreeView
-//         onNodeSelect={handleNodeSelect}
-//         className={classes.root}
-//         defaultCollapseIcon={<ExpandMoreIcon />}
-//         defaultExpandIcon={<ChevronRightIcon />}
-//       >
-//         {data.map((node) => renderTree(node, searchText, expandedNodes, handleNodeToggle))}
-//       </TreeView>
-//     </Paper>
-//   );
-// };
-
 
 export default TreeViewDemo;
