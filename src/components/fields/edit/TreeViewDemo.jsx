ff9814775eacba14551b1f6d53099dcc0f1ba0a3
erinz2020 <erinz2020@gmail.com>
2023-11-28 15:32:28 +0000

    lots of work on hierachy

--- src/components/fields/edit/TreeViewDemo.jsx
+++ src/components/fields/edit/TreeViewDemo.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TreeView from '@material-ui/lab/TreeView';
 import TreeItem from '@material-ui/lab/TreeItem';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Radio from '@material-ui/core/Radio';
-import Paper from '@material-ui/core/Paper';
 import _ from 'lodash-es';
 
 
@@ -31,10 +30,12 @@ const TreeViewDemo = (props) => {
   const {    
     onChange,    
     searchText,
-    showData
+    showData,
+    selected,
+    setSelected,
   } = props;
   const classes = useStyles();
-  const [selected, setSelected] = useState(null);
+  
 
   const handleNodeSelect = (event, nodeId) => {
     onChange(nodeId);
@@ -45,7 +46,7 @@ const TreeViewDemo = (props) => {
     return (
       <TreeItem
         key={node.id}
-        nodeId={node.id}
+        nodeId={node.id}        
         label={
           <div style={{ 
             width: '100%', 
@@ -66,20 +67,22 @@ const TreeViewDemo = (props) => {
 
 
   return (
-    <Paper style={{maxHeight:1200, width: '100%', overflow:"auto"}}>
+    <div 
+      
+      style={{maxHeight:1200, width: '100%', overflow:"auto"}}
+      data-testid="treeViewDemo"
+      >
       <TreeView
         onNodeSelect={handleNodeSelect}
-
         className={classes.root}
         defaultCollapseIcon={<ExpandMoreIcon />}
         defaultExpandIcon={<ChevronRightIcon />}
       >
-        {/* {data.map((node) => renderTree(node, searchText))} */}
         {(_.isNil(showData) || !_.isArray(showData) || _.isEmpty(showData)) 
           ? <></> 
           : showData.map(node => renderItem(node))}
       </TreeView>
-    </Paper>
+    </div>
   );
 };
 
