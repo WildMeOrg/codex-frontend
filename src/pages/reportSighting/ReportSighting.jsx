3f054e2f5ed4015df4b391aa14bf37ab97eb79d2
TanyaStere42 <tstere@wildme.org>
2024-08-08 12:01:10 -0700

    update docs and donate links

--- src/pages/reportSighting/ReportSighting.jsx
+++ src/pages/reportSighting/ReportSighting.jsx
@@ -150,7 +150,7 @@ export default function ReportSighting({ authenticated }) {
                 <FormattedMessage id="PHOTO_OPTIMIZE_1" />
                 <Link
                   external
-                  // href="https://docs.wildme.org/product-docs/en/codex/data/optimizing-photographs/"
+                  // href="https://codex.docs.wildme.org/data/optimizing-photographs.html"
                   href={photoGuidelinesUrl}
                   target="_blank"
                 >
